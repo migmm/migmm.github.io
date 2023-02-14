fa2527933d9e3f921920e7d24f5aa7c6a68d377b
migmm <micmigm@gmail.com>
2023-02-13 22:06:37 -0300

    HTML courses created

--- frontend/src/components/Certificates/Certificates.tsx
+++ frontend/src/components/Certificates/Certificates.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Certificates = () => {
-    return <>Certificates</>;
+    return (
+        <>
+            <div className="certifications-container">
+                <h1>Certifications</h1>
+                <div className="selectors-container">
+                    <select name="select">
+                        <option value="value3" selected>select</option>
+                        <option value="linkedin">linkedin</option>
+                        <option value="udemy" >udemy</option>
+                    </select>
+                </div>
+                <div className="cards-container">
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Certificates;
