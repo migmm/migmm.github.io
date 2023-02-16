0b7780f9113b5e9d50ad5ad7498c7aaeccf056fa
migmm <micmigm@gmail.com>
2023-02-16 18:15:05 -0300

    Added certificates sctructure

--- frontend/src/components/Certificates/Certificates.tsx
+++ frontend/src/components/Certificates/Certificates.tsx
@@ -1,15 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 const Certificates = () => {
     return (
-        <>
+        <CertificatesStyles>
             <div className="certifications-container">
                 <h1>Certifications</h1>
                 <div className="selectors-container">
-                    <select name="select">
-                        <option value="value3" selected>select</option>
+                    <label htmlFor="select-company">Company</label>
+                    <select name="select" className="select-company" id="select-company" >
+                        <option value="value3" selected>
+                            select
+                        </option>
                         <option value="linkedin">linkedin</option>
-                        <option value="udemy" >udemy</option>
+                        <option value="udemy">udemy</option>
+                    </select>
+                    <label htmlFor="select-category">Category</label>
+                    <select name="select" className="select-category" id="select-category" >
+                        <option value="value3" selected>
+                            select
+                        </option>
+                        <option value="linkedin">nodejs</option>
+                        <option value="udemy">scrum</option>
                     </select>
                 </div>
                 <div className="cards-container">
@@ -19,10 +31,198 @@ const Certificates = () => {
                         </div>
                         <h2>Course</h2>
                     </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        <h2>Course</h2>
+                    </div>
+                </div>
+                <div className="pagination-container">
+                    <span className="pages"><span className="pages">1</span><span className="pages">2</span><span className="pages">3</span></span>
+                <select name="select" className="select-page" id="select-page" >
+                        <option value="value3" selected>
+                            select
+                        </option>
+                        <option value="linkedin">10</option>
+                        <option value="udemy">20</option>
+                    </select>
                 </div>
             </div>
-        </>
+        </CertificatesStyles>
     );
 };
 
 export default Certificates;
+
+const CertificatesStyles = styled.header`
+
+`;
