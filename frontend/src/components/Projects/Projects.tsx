49be5c8e293fdedc72a9fdbbf5a709deb35aad98
migmm <micmigm@gmail.com>
2023-02-17 19:18:08 -0300

    HTML structure to projects

--- frontend/src/components/Projects/Projects.tsx
+++ frontend/src/components/Projects/Projects.tsx
@@ -1,9 +1,121 @@
 import React from 'react'
+import styled from "styled-components";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
+    return (
+        <ProjectsStyles>
+            <h1>Projects</h1>
+            <h2>latest project</h2>
+            <div className="latest-project-container">
+                <div className="latest-project-image-container"></div>
+                <div className="latest-project-description"></div>
+            </div>
+            <div className="projects-container">
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image-container">
+                    </div>
+                    <div className="card-title-container">
+                        <h1 className="title">Project title</h1>
+                    </div>
+                    <div className="card-languages-container">
+                        <span>React</span>
+                        <span>NodeJS</span>
+                        <span>MongoDB</span>
+                    </div>
+                </div>
+            </div>
+        </ProjectsStyles>
+    )
 }
 
 export default Projects
+
+
+const ProjectsStyles = styled.header`
+
+
+`
