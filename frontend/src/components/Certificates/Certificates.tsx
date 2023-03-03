092fcbdb9ef9c0479deb556adcc7461714935751
migmm <micmigm@gmail.com>
2023-03-02 22:51:15 -0300

    Styled contact section

--- frontend/src/components/Certificates/Certificates.tsx
+++ frontend/src/components/Certificates/Certificates.tsx
@@ -9,7 +9,7 @@ const Certificates = () => {
                 <div className="selectors-container">
                     <span>Browse by</span>
                     {/* <label htmlFor="select-company">Company</label> */}
-                    <select name="select" className="select-company" id="select-company" >
+                    <select name="select" className="select-company" id="select-company">
                         <option value="value3" selected>
                             Company
                         </option>
@@ -18,53 +18,55 @@ const Certificates = () => {
                     </select>
                     <span>or</span>
                     {/* <label htmlFor="select-category">Category</label> */}
-                    <select name="select" className="select-category" id="select-category" >
+                    <select name="select" className="select-category" id="select-category">
                         <option value="value3" selected>
-                        Category
+                            Category
                         </option>
                         <option value="linkedin">nodejs</option>
                         <option value="udemy">scrum</option>
                     </select>
                 </div>
                 <div className="cards-container">
+                    {/* CARD */}
+                    <div className="card">
+                        <div className="image-container">
+                            <img src="" alt="" />
+                        </div>
+                        {/*  <div className="img-miniature-container">
+                            <img src="" alt="" />
+                        </div> */}
+                        <h2>Course</h2>
+                    </div>
 
                     {/* CARD */}
                     <div className="card">
                         <div className="image-container">
                             <img src="" alt="" />
                         </div>
-                       {/*  <div className="img-miniature-container">
+                        {/*  <div className="img-miniature-container">
                             <img src="" alt="" />
                         </div> */}
                         <h2>Course</h2>
                     </div>
 
-                         {/* CARD */}
-                        <div className="card">
+                    {/* CARD */}
+                    <div className="card">
                         <div className="image-container">
                             <img src="" alt="" />
                         </div>
-                       {/*  <div className="img-miniature-container">
+                        {/*  <div className="img-miniature-container">
                             <img src="" alt="" />
                         </div> */}
                         <h2>Course</h2>
                     </div>
-
-                         {/* CARD */}
-                        <div className="card">
-                        <div className="image-container">
-                            <img src="" alt="" />
-                        </div>
-                       {/*  <div className="img-miniature-container">
-                            <img src="" alt="" />
-                        </div> */}
-                        <h2>Course</h2>
-                    </div>
-                    
                 </div>
                 <div className="pagination-container">
-                    <span className="pages">Pages <span className="pages">1</span><span className="pages">2</span><span className="pages">3</span></span>
-                <select name="select" className="select-page" id="select-page" >
+                    <span className="pages">
+                        Pages <span className="pages">1</span>
+                        <span className="pages">2</span>
+                        <span className="pages">3</span>
+                    </span>
+                    <select name="select" className="select-page" id="select-page">
                         <option value="value3" selected>
                             Qty per page
                         </option>
@@ -80,84 +82,83 @@ const Certificates = () => {
 export default Certificates;
 
 const CertificatesStyles = styled.header`
+    max-width: 1900px;
+    margin: 1em;
 
-max-width: 1900px;
-margin: 1em;
+    .certifications-container {
+        h1 {
+            font-family: "Work Sans", sans-serif;
+            font-weight: 800;
+            text-align: center;
+            margin: 1em 1em 0.5em 1em;
+        }
 
-.certifications-container {
-    h1 {
-        font-family: 'Work Sans', sans-serif;
-        font-weight: 800;
-        text-align: center;
-        margin: 1em 1em .5em 1em;
-    }
-
-    .selectors-container {
-        text-align: center;
-        font-family: 'Work Sans', sans-serif;
-        font-weight: 600;
-        margin-bottom: 1em;
-        font-size: 1.3em;
-        background-color: #ffffff;
-        border-radius: 10px;
-        padding: .5em;
-
-        .select-company,
-        .select-category {
-            font-family: 'Work Sans', sans-serif;
+        .selectors-container {
+            text-align: center;
+            font-family: "Work Sans", sans-serif;
             font-weight: 600;
-            border: none;
-            font-size: 1em;
-            cursor: pointer;
-        }
+            margin-bottom: 1em;
+            font-size: 1.3em;
+            background-color: #ffffff;
+            border-radius: 10px;
+            padding: 0.5em;
 
-        .select-company:hover,
-        .select-category:hover {
-            color: grey
-        }
-    }
-
-    .cards-container {
-        display: grid;
-        grid-template-columns: 1fr;
-        gap: 1em;
-
-        .card {
-            width: 100%;
-            height: 200px;
-            margin-bottom: 2em;
-            
-            .image-container {
-                background-color: #0069c5;
-                height: 150px;
-                border-radius: 30px;
+            .select-company,
+            .select-category {
+                font-family: "Work Sans", sans-serif;
+                font-weight: 600;
+                border: none;
+                font-size: 1em;
+                cursor: pointer;
             }
 
-            h2 {
-                font-family: 'Work Sans', sans-serif;
-                font-weight: 700;
-                margin-top: .5em;
+            .select-company:hover,
+            .select-category:hover {
+                color: grey;
+            }
+        }
+
+        .cards-container {
+            display: grid;
+            grid-template-columns: 1fr;
+            gap: 1em;
+
+            .card {
+                width: 100%;
+                height: 200px;
+                margin-bottom: 2em;
+
+                .image-container {
+                    background-color: #0069c5;
+                    height: 150px;
+                    border-radius: 30px;
+                }
+
+                h2 {
+                    font-family: "Work Sans", sans-serif;
+                    font-weight: 700;
+                    margin-top: 0.5em;
+                }
+            }
+        }
+
+        .pagination-container {
+            text-align: center;
+            font-family: "Work Sans", sans-serif;
+            font-weight: 600;
+            background-color: #ffffff;
+            border-radius: 10px;
+            padding: 0.5em;
+            margin-bottom: 1em;
+            font-size: 1.3em;
+
+            select {
+                display: none;
+                border: none;
+                font-family: "Work Sans", sans-serif;
+                font-weight: 600;
+                font-size: 1em;
             }
         }
     }
-
-    .pagination-container {
-        text-align: center;
-        font-family: 'Work Sans', sans-serif;
-        font-weight: 600;
-        background-color: #ffffff;
-        border-radius: 10px;
-        padding: .5em;
-        margin-bottom: 1em;
-        font-size: 1.3em;
-
-        select {
-            display:none;
-            border: none;
-            font-family: 'Work Sans', sans-serif;
-            font-weight: 600;
-            font-size: 1em;
-        }
-    }
-}
 `;
--- frontend/src/components/Contact/Contact.tsx
+++ frontend/src/components/Contact/Contact.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 function Contact() {
-    return <ContactContainer>
-
-    <h1>Contact</h1>
-            <div className="first">
-                <p>You can contact me sending a form or in a links below</p>
+    return (
+        <ContactContainer>
+            <h1>Contact</h1>
+            <div className="column-container">
+                <p className="column-text">You can contact me sending a mail to hi@miguedev.com or completing the form below.</p>
                 <div className="form-container">
                     <form action="#">
                         <div className="input-group">
@@ -33,16 +33,93 @@ function Contact() {
                             </label>
                             <textarea name="message" id="message"></textarea>
                         </div>
+                        <div className="input-group">
+                            <button type="submit">Submit</button>
+                            <button type="reset">Reset</button>
+                        </div>
                     </form>
                 </div>
             </div>
-            
-    </ContactContainer>;
+        </ContactContainer>
+    );
 }
 
 export default Contact;
 
-
 const ContactContainer = styled.header`
+    max-width: 1900px;
+    margin: 1em;
+    h1 {
+        font-family: "Work Sans", sans-serif;
+        font-weight: 800;
+        text-align: center;
+        margin: 1em 1em 0.5em 1em;
+    }
+    .column-container {
+        .column-text {
+            font-family: "Work Sans", sans-serif;
+            font-weight: 600;
+            text-align: center;
+            margin: 1em 1em 0.5em 1em;
+        }
+        .form-container {
+            .input-group {
+                display: flex;
+                flex-direction: column;
 
-`
+                label {
+                    font-family: "Work Sans", sans-serif;
+                    font-weight: 600;
+                    text-align: center;
+                    margin-bottom: .5em;
+                }
+
+                input, textarea {
+                    font-family: "Work Sans", sans-serif;
+                    font-weight: 600;
+                    text-align: center;
+                    font-size: 1em;
+                    margin-bottom: 1em;
+                    border-radius: 20px;
+                    padding: .5em;
+                    border: 1px solid #ebebeb;
+                    :focus {
+                        background-color: #e2e2e2;
+                    }
+                }
+
+                textarea {
+                    resize: vertical;
+                    height: 10em;
+                }
+
+                button {
+                    border-radius: 20px;
+                    padding: 1em;
+                    border: 1px solid #ebebeb;
+                    background-color: #0069c5;
+                    color: #ffffff;
+                    font-family: "Work Sans", sans-serif;
+                    font-weight: 600;
+                    text-align: center;
+                    font-size: 1em;
+                    width: 50%;
+                    cursor: pointer;
+
+                    :hover {
+                        background-color: #004a8b;
+                    }
+
+                    :active {
+                        background-color: #0088ff;
+                    }
+                }
+            }
+            .input-group:last-child {
+                    display: flex;
+                    flex-direction: row;
+                    gap: 1em;
+                }
+        }
+    }
+`;
