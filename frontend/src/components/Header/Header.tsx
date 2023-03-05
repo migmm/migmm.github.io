cd025b11e192d886322b780420dd0976acbfba42
migmm <micmigm@gmail.com>
2023-03-04 21:55:52 -0300

    Fixed margin in navbar

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,25 +4,27 @@ import NavBar from "./Navbar/NavBar";
 import styled from "styled-components";
 
 const Header = () => (
-    <HeaderContainer>
-        <header>
-            <div className="logo-container">
-                <img src="img/logo.png" alt="Logo" />
-            </div>
-            <div className="navbar-container">
-                <NavBar />
-            </div>
-            <span className="contact-button">Contact me!</span>
-            <div className="hamburger-button-container">
-                <HamburgerButton />
-            </div>
-        </header>
-    </HeaderContainer>
+    <HeaderStyles>
+        <div className="header-container">
+            <header>
+                <div className="logo-container">
+                    <img src="img/logo.png" alt="Logo" />
+                </div>
+                <div className="navbar-container">
+                    <NavBar />
+                </div>
+                <span className="contact-button">Contact me!</span>
+                <div className="hamburger-button-container">
+                    <HamburgerButton />
+                </div>
+            </header>
+        </div>
+    </HeaderStyles>
 );
 
 export default Header;
 
-const HeaderContainer = styled.header`
+const HeaderStyles = styled.header`
     max-width: 1900px;
     display: flex;
     justify-content: center;
@@ -30,66 +32,70 @@ const HeaderContainer = styled.header`
     height: 5.5em;
     border: 1px solid #ebebeb; //#E3E3E3
     border-radius: 2.75em;
-    margin: 1em auto;
+    margin: 0 auto;
 
-    header {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        width: 1600px;
-        padding: 0 2em;
-    }
+    .header-container {
+        margin: 1em;
 
-    .logo-container {
-        width: 65px;
-        height: 65px;
-        flex-grow: 1;
-        cursor: pointer;
-
-        img {
-            //width: 100%;
-            height: 100%;
-            object-fit: contain;
+        header {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            width: 1600px;
+            padding: 0 2em;
         }
-    }
 
-    .navbar-container {
-        display: none;
-        @media (min-width: 768px) {
-            display: block;
+        .logo-container {
+            width: 65px;
+            height: 65px;
+            flex-grow: 1;
+            cursor: pointer;
+
+            img {
+                //width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
         }
-    }
 
-    .contact-button {
-        background-color: #ED1B23;
-        height: 65px;
-        font-family: "Work Sans", sans-serif;
-        color: white;
-        font-weight: 500;
-        line-height:65px;
-        font-size: 1em;
-        padding: 0 1em;
-        border-radius: 20px;
-        margin-right: 1em;
-        cursor: pointer;
-
-        @media (min-width: 768px) {
+        .navbar-container {
             display: none;
+            @media (min-width: 768px) {
+                display: block;
+            }
         }
 
-        :hover {
-            background-color: #ce030a;
+        .contact-button {
+            background-color: #ed1b23;
+            height: 65px;
+            font-family: "Work Sans", sans-serif;
+            color: white;
+            font-weight: 500;
+            line-height: 65px;
+            font-size: 1em;
+            padding: 0 1em;
+            border-radius: 20px;
+            margin-right: 1em;
+            cursor: pointer;
+
+            @media (min-width: 768px) {
+                display: none;
+            }
+
+            :hover {
+                background-color: #ce030a;
+            }
+
+            :active {
+                background-color: #ff0000;
+            }
         }
 
-        :active {
-            background-color: #ff0000;
-        }
-    }
-
-    .hamburger-button-container {
-        @media (min-width: 768px) {
-            display: none;
+        .hamburger-button-container {
+            @media (min-width: 768px) {
+                display: none;
+            }
         }
     }
 `;
