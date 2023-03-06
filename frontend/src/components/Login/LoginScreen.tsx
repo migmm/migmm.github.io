b5bd617320b5248faa522f1830a1bb098f9dd54a
migmm <micmigm@gmail.com>
2023-03-05 22:57:07 -0300

    Added login boilerplate

---
+++ frontend/src/components/Login/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components";
+
+const LoginScreen = () => {
+  return (
+    <LoginScreenStyles>LoginScreen</LoginScreenStyles>
+  )
+}
+
+export default LoginScreen
+
+const LoginScreenStyles = styled.div ``
