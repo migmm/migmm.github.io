1dd09286e7a798f4bff38a48fc4fec7579921881
migmm <micmigm@gmail.com>
2023-03-05 23:04:06 -0300

    Added Footer bolierplate

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components"
+
+const Footer = () => {
+    return <FooterStyles>Footer</FooterStyles>;
+};
+
+const FooterStyles = styled.footer``;
