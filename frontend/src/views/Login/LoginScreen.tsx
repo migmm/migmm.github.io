46b9577ef45d6f67a19cef9fb93e7f47ab03e20d
migmm <micmigm@gmail.com>
2023-03-07 18:20:32 -0300

    Created styles

--- frontend/src/views/Login/LoginScreen.tsx
+++ frontend/src/views/Login/LoginScreen.tsx
@@ -37,5 +37,78 @@ const LoginScreen = () => {
 export default LoginScreen;
 
 const LoginScreenStyles = styled.main`
+    max-width: 1900px;
+    margin: 0 auto;
+    margin: 1em;
 
+    h1 {
+        font-family: "Work Sans", sans-serif;
+        font-weight: 800;
+        text-align: center;
+        margin: 1em 1em 0.5em 1em;
+    }
+
+    .login-form-container {
+        .input-group {
+            display: flex;
+            flex-direction: column;
+
+            label {
+                font-family: "Work Sans", sans-serif;
+                font-weight: 600;
+                text-align: center;
+                margin-bottom: 0.5em;
+            }
+
+            input {
+                font-family: "Work Sans", sans-serif;
+                font-weight: 600;
+                text-align: center;
+                font-size: 1em;
+                margin-bottom: 1em;
+                border-radius: 20px;
+                padding: 0.5em;
+                border: 1px solid #ebebeb;
+
+                :focus {
+                    background-color: #e2e2e2;
+                }
+            }
+        }
+        .register-container {
+            margin: 1em 1em 1em 1em;
+            span {
+                font-family: "Work Sans", sans-serif;
+                font-weight: 600;
+                text-align: center;
+            }
+        }
+        button {
+            border-radius: 20px;
+            padding: 1em;
+            border: 1px solid #ebebeb;
+            background-color: #0069c5;
+            color: #ffffff;
+            font-family: "Work Sans", sans-serif;
+            font-weight: 600;
+            text-align: center;
+            font-size: 1em;
+            width: 50%;
+            cursor: pointer;
+
+            :hover {
+                background-color: #004a8b;
+            }
+
+            :active {
+                background-color: #0088ff;
+            }
+        }
+    }
+
+    .input-group:last-child {
+        display: flex;
+        flex-direction: row;
+        gap: 1em;
+    }
 `;
