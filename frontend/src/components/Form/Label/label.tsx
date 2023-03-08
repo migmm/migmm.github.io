42ca98d6780d2c00d97bffaedcf12702f3fbd126
migmm <micmigm@gmail.com>
2023-03-08 14:51:39 -0300

    Created label component

---
+++ frontend/src/components/Form/Label/label.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+
+export const Label = styled.label`
+
+    font-family: "Work Sans", sans-serif;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 0.5em;
+`
