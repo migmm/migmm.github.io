2b3cdccc7525410e0ff72a198b9cd9037185fe4e
migmm <micmigm@gmail.com>
2023-03-08 15:38:09 -0300

    Created textarea component

---
+++ frontend/src/components/Form/Textarea/Textarea.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Textarea = styled.textarea`
+
+    font-family: "Work Sans", sans-serif;
+    font-weight: 600;
+    text-align: center;
+    font-size: 1em;
+    margin-bottom: 1em;
+    border-radius: 20px;
+    padding: 0.5em;
+    border: 1px solid #ebebeb;
+    resize: vertical;
+    height: 10em;
+
+    :focus {
+        background-color: #e2e2e2;
+    }
+`;
