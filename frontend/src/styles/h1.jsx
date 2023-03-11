a74e415299273a1128a544d75f0e106ac38fd798
migmm <micmigm@gmail.com>
2023-03-11 20:15:05 -0300

    Added styles to h1 styled component

--- frontend/src/styles/h1.jsx
+++ frontend/src/styles/h1.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const Input = styled.h1`
-
+    font-family: "Work Sans", sans-serif;
+    font-weight: 800;
+    text-align: center;
+    margin: 1em 1em 0.5em 1em;
 `;
