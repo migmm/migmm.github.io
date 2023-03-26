ec38f0767e6b8cb254112319fa0106031ee80ce9
migmm <micmigm@gmail.com>
2023-03-25 22:32:42 -0300

    Partial styles

--- frontend/src/views/AddCertificate/AddCertificate.tsx
+++ frontend/src/views/AddCertificate/AddCertificate.tsx
@@ -11,25 +11,23 @@ const AddCertificate = () => {
         <AddScreenStyles>
             <div className="certification-container">
                 <H1>Add Certification</H1>
-                <div className="Add-form-container">
+                <div className="add-form-container">
                     <form action="#">
                         <div className="input-group">
                             <Label htmlFor="certification-name">Certification title</Label>
                             <Input type="text" id="certificationName" name="certificationName" />
                             <LabelError>Error</LabelError>
                             <Label htmlFor="certification-vendor">Vendor</Label>
-                            <textarea name="certificationVendor" id="certification-vendor"></textarea>
+                            <Input type="text" name="certificationVendor" id="certification-vendor"></Input>
                             <LabelError>Error</LabelError>
                             <Label htmlFor="certification-url">URL</Label>
-                            <Input type="certification-url" id="certification-url" name="certificationUrl" />
+                            <Input type="text" id="certification-url" name="certificationUrl" />
                             <LabelError>Error</LabelError>
                             <Label htmlFor="certification-description">Description</Label>
                             <textarea name="certificationDescription" id="certification-description"></textarea>
                             <LabelError>Error</LabelError>
                             <Label htmlFor="certification-description">Image</Label>
-                            <input type="file" name="certificationImage" id="certification-image">
-                                Add image
-                            </input>
+                            <input type="file" id="certification-image" name="certificationImage" accept="image/png, image/jpeg"></input>
                             <LabelError>Error</LabelError>
                         </div>
                         <div className="input-group">
@@ -45,4 +43,21 @@ const AddCertificate = () => {
 
 export default AddCertificate;
 
-const AddScreenStyles = styled.main``;
+const AddScreenStyles = styled.main`
+    max-width: 1900px;
+    margin: 0 auto;
+    .certification-container {
+
+        margin: 1em;
+
+        .add-form-container {
+            form {
+                .input-group {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                }
+            }
+        }
+    }
+`;
