e1fd32b3cbfbc710340cc01d13a903cff404b2f6
migmm <micmigm@gmail.com>
2023-04-11 21:34:33 -0300

    Fixing errors

--- backend/src/controller/certifications.ts
+++ backend/src/controller/certifications.ts
@@ -1,55 +1,50 @@
-import api from '../api/certifications';
-
+import api from "../api/certifications";
 
 ////////////////////////////////////////////////////////////////////////////////
 //                               GET Controllers                              //
 ////////////////////////////////////////////////////////////////////////////////
 
-const getCertifications = async (_req:any, res:any) => {
+const getCertifications = async (_req: any, res: any) => {
     res.json(await api.getCertifications());
 };
 
-const getCertification = async (req:any, res:any) => {
+const getCertification = async (req: any, res: any) => {
     const id = req.params.id;
-    res.json(await api.getCertification(id, res));
+    res.json(await api.getCertification(id));
 };
-
 
 ///////////////////////////////////////////////////////////////////////////////
 //                              POST Controllers                             //
 ///////////////////////////////////////////////////////////////////////////////
 
-const postCertification = async (req:any, res:any) => {
+const postCertification = async (req: any, res: any) => {
     let certification = req.body;
-    const newCertification = await api.createCertification(certification, res);
+    const newCertification = await api.createCertification(certification);
     res.json(newCertification);
 };
-
 
 //////////////////////////////////////////////////////////////////////////////
 //                              PUT Controllers                             //
 //////////////////////////////////////////////////////////////////////////////
 
-const putCertification = async (req:any, res:any) => {
+const putCertification = async (req: any, res: any) => {
     const id = req.params.id;
     const certification = req.body;
 
-    const updatedCertification = await api.updateCertification(id, certification, res) || {};
+    const updatedCertification = (await api.updateCertification(id, certification)) || {};
     res.json(updatedCertification);
 };
-
 
 ///////////////////////////////////////////////////////////////////////////////
 //                             DELETE Controllers                            //
 ///////////////////////////////////////////////////////////////////////////////
 
-const deleteCertification = async (req:any, res:any) => {
+const deleteCertification = async (req: any, res: any) => {
     const id = req.params.id;
 
-    const removedCertification = await api.deleteCertification(id, res) || {};
+    const removedCertification = (await api.deleteCertification(id)) || {};
     res.json(removedCertification);
 };
-
 
 export default {
     getCertifications,
