93b97667bb356bea64e01d85ffbcb63401d32c9d
migmm <micmigm@gmail.com>
2023-04-14 21:19:41 -0300

    Added user file

---
+++ backend/src/model/users/users.ts
@@ -0,0 +1,17 @@
+import config, {PERSISTENCE_TYPES} from '../../config';
+
+import UserModelMongoDB from './users-mongodb';
+
+class UserModel {
+    static get(type:any) {
+        console.log(`#### Persistence -> ${config.PERSISTENCE_TYPE || 'by default'} ####`);
+        switch (type) {
+            case PERSISTENCE_TYPES.TYPE_MONGODB:
+                return new UserModelMongoDB();
+            default:
+                return new UserModelMongoDB();
+        }
+    }
+}
+
+export default UserModel;
