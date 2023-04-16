16bd1e3ee67c370be156732d18f4f8eee46f694e
migmm <micmigm@gmail.com>
2023-04-16 18:46:41 -0300

    Added auth controller

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import api from '../api/users';
-
+import bcrypt from "bcrypt";
 
 ////////////////////////////////////////////////////////////////////////////////
 //                               GET Controllers                              //
@@ -34,14 +34,31 @@ const getUser = async (req:Request, res:Response) => {
 ///////////////////////////////////////////////////////////////////////////////
 
 const postUser = async (req:Request, res:Response) => {
-    let user = req.body;
+    const { username, password } = req.body
 
-    try {
+    if (!username || !password) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    const foundUser = await api.getUser(username);
+    console.log(foundUser)
+    if (!foundUser) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const match = await bcrypt.compare(password, foundUser.password)
+
+    if (!match) return res.status(401).json({ message: 'Unauthorized' })
+
+
+    res.status(201).json(foundUser);
+console.log(foundUser)
+    /* try {
         const newUser = await api.createUser(user);
         res.status(201).json(newUser);
     } catch (error) {
         res.status(500).send('Error posting users')
-    }
+    } */
 };
 
 
