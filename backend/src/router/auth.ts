67100b7e2d9c3ceb0ca1357ac2caddbaf1f10e13
migmm <micmigm@gmail.com>
2023-04-17 18:27:59 -0300

    Cleaning code

--- backend/src/router/auth.ts
+++ backend/src/router/auth.ts
@@ -3,10 +3,7 @@ import authController from '../controller/auth';
 
 const routerAuth = express.Router();
 
-routerAuth.get('/', authController.getAuth);
-routerAuth.get('/:id', authController.getAuth);
+routerAuth.get('/:user', authController.getAuth);
 routerAuth.post('/', authController.postAuth);
-routerAuth.put('/:id', authController.putAuth);
-routerAuth.delete('/:id', authController.deleteAuth);
 
 export default routerAuth;
