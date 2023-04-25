c76a9224c116d203675b72df3ad510b657abf319
migmm <micmigm@gmail.com>
2023-04-24 21:52:48 -0300

    Working login limiter

--- backend/src/middlewares/loginLimiter.ts
+++ backend/src/middlewares/loginLimiter.ts
@@ -1,18 +1,18 @@
-import { Request, Response, NextFunction } from "express";
-import rateLimit from 'express-rate-limit';
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import rateLimit from "express-rate-limit";
 
-const loginLimiter = rateLimit({
-    windowMs: 60 * 1000, 
+const loginLimiter: RequestHandler = rateLimit({
+    windowMs: 60 * 1000,
     max: 3,
-    message:
-        { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
-        handler: (req:Request, res:Response, next:NextFunction, options:any) => {
-        res.status(options.statusCode).send(options.message)
+    message: {
+        message: "Too many login attempts from this IP, please try again after a 60 second pause",
     },
-    standardHeaders: true, 
-    legacyHeaders: false,
-})
+    handler: (req: Request, res: Response, _next: NextFunction) => {
+        const rateLimitMessage = (req as any).rateLimit.message;
+        res.status(429).json({ error: rateLimitMessage });
+    },
+    statusCode: 429,
+    headers: true,
+});
 
-export default { 
-    loginLimiter 
-};
+export default loginLimiter;
--- backend/src/router/auth.ts
+++ backend/src/router/auth.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import authController from '../controller/auth';
+import loginLimiter from '../middlewares/loginLimiter';
 
 const routerAuth = express.Router();
 
 routerAuth.get('/:user', authController.getAuth);
-routerAuth.post('/', authController.postAuth);
+routerAuth.post('/', loginLimiter as any, authController.postAuth);
 routerAuth.post('/logout', authController.logout);
-
 
 export default routerAuth;
