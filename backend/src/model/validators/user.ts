dacd4a736f3482397b0f656011d39c154cb20ccc
migmm <micmigm@gmail.com>
2023-04-26 10:55:09 -0300

    Added role validations to joi

--- backend/src/model/validators/user.ts
+++ backend/src/model/validators/user.ts
@@ -1,12 +1,12 @@
 import Joi from "joi";
-
+import allowedRoles from '../../config/userRoles';
 class ProjectValidator {
     static validate(project: object) {
         const projectSchema = Joi.object({
             username: Joi.string().required(),
             password: Joi.string().min(6).max(1000).required(),
             mail: Joi.string().required(),
-            role: Joi.string().required(),
+            role: Joi.string().valid(...allowedRoles).required(),
         });
         const { error } = projectSchema.validate(project);
         return error;
