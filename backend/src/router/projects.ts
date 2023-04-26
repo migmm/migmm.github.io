0542f27de815f87f6ee880bf45a8bcfe7b5990a6
migmm <micmigm@gmail.com>
2023-04-25 22:00:43 -0300

    Added role middleware to routes

--- backend/src/router/projects.ts
+++ backend/src/router/projects.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import projectsController from '../controller/projects';
 import multerFS from '../middlewares/multer';
+import authRole from '../middlewares/authRole';
 
 const routerProjects = express.Router();
 
-routerProjects.get('/', projectsController.getProjects);
+routerProjects.get('/', authRole(['admin', 'user']) as any,projectsController.getProjects);
 routerProjects.get('/:id', projectsController.getProject);
 routerProjects.post('/', multerFS.fieldConfig, projectsController.postProject);
 routerProjects.put('/:id', multerFS.fieldConfig, projectsController.putProject);
