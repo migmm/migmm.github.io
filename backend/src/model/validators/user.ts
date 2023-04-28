5a0ac43355366b12fc107685dd62e817f59181f9
migmm <micmigm@gmail.com>
2023-04-28 13:52:32 -0300

    Changed validation reqired

--- backend/src/model/validators/user.ts
+++ backend/src/model/validators/user.ts
@@ -6,8 +6,8 @@ class ProjectValidator {
             username: Joi.string().required(),
             password: Joi.string().min(6).max(1000).required(),
             mail: Joi.string().required(),
-            role: Joi.string().valid(...allowedRoles).required(),
-            banned: Joi.boolean().required().default(false)
+            role: Joi.string().valid(...allowedRoles).default('user'),
+            banned: Joi.boolean().default(false)
         });
         const { error } = projectSchema.validate(project);
         return error;
