909297616787578d28bf8eca9fb7c7e9e40cc35b
migmm <micmigm@gmail.com>
2023-04-29 23:09:20 -0300

    Fully working auth roles

--- backend/src/middlewares/authRole.ts
+++ backend/src/middlewares/authRole.ts
@@ -19,7 +19,7 @@ const authRole = (roles: string[]) => async (req: Request & { currentUser: UserP
 
         // Ckeck if you have a token, if not, you are unauthorized
         if (!token) {
-            return res.status(401).json({ message: "Unauthorized" });
+            return res.status(401).json({ message: "Unauthorized." });
         }
 
         // Decode token
@@ -39,33 +39,33 @@ const authRole = (roles: string[]) => async (req: Request & { currentUser: UserP
 
         // First check if user is admin or has required role specified in the route
         if (!roles.includes(userAuth.role)) {
-            return res.status(403).json({ message: "Forbidden" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
         // Check if user is trying to modify their own information
         // If admin is logged just go on
         if (id && id !== userAuth.id && userAuth.role !== "admin") {
-            return res.status(403).json({ message: "Forbidden" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
           // Check if user has permission to get
         if (req.body && req.method === "GET" && !rolesPermissions[userAuth.role].canGet) {
-            return res.status(403).json({ message: "Forbidden" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
         // Check if user has permission to update
         if (req.body && req.method === "PUT" && !rolesPermissions[userAuth.role].canUpdate) {
-            return res.status(403).json({ message: "Forbidden" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
         // Check if user has permission to delete
         if (req.method === "DELETE" && !rolesPermissions[userAuth.role].canDelete) {
-            return res.status(403).json({ message: "Forbidden" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
         // Check if user has permission to create
         if (req.body && req.method === "POST" && !rolesPermissions[userAuth.role].canCreate) {
-            return res.status(403).json({ message: "Forbidden" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
         // If banned property is in body
@@ -73,7 +73,7 @@ const authRole = (roles: string[]) => async (req: Request & { currentUser: UserP
         // Or if role permission can change banned status
         // User check is negation in order to throw an error if is true
         if (req.body.hasOwnProperty("banned") && (userAuth.role !== "admin" || !rolesPermissions[userAuth.role].canBan)) {
-            return res.status(403).json({ message: "Forbidden 3" });
+            return res.status(403).json({ message: "Forbidden." });
         }
 
         // If role property is in body
