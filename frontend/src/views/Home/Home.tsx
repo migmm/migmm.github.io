5ac46397f3f86f076b2a7cd134a8ef344a7c3115
migmm <micmigm@gmail.com>
2023-05-08 14:33:48 -0300

    Fixed scroll affecting all pages

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -10,59 +10,62 @@ import logoLinkedin from "./linkedin.svg";
 import { Paragraph } from "../../Styles/Paragraph/Paragraph";
 
 const Home = () => {
+  useEffect(() => {
     let url = window.location.pathname;
     url = url.slice(1, 1);
 
-    console.log(url);
+    const lockScroll = (option: string) => {
+        console.log(url);
+        if (option === "enabled") {
+            document.body.style.overflow = "hidden";
+            document.body.style.userSelect = "none";
+        } else {
+            document.body.style.overflow = "auto";
+            document.body.style.userSelect = "auto";
+        }
+    };
 
-    if (!url) {
-        const lockScroll = (option: string) => {
-            console.log(url);
-            if (option === "enabled") {
-                document.body.style.overflow = "hidden";
-                document.body.style.userSelect = "none";
-            } else {
-                document.body.style.overflow = "auto";
-                document.body.style.userSelect = "auto";
+    const zoomElement = document.querySelector(".text-home") as HTMLElement;
+    const contactIcons = document.querySelector(".contact-icons") as HTMLElement;
+    const OPACITY_STEP = 0.1;
+    const ZOOM_SPEED = 0.2;
+    let opacity = 1;
+    let zoom = 1;
+    let y = 0;
+
+    function handleScroll (e:any){
+        if (window.pageYOffset <= 0) {
+            lockScroll("enabled");
+            zoomElement.style.display = `block`;
+        }
+
+        if (e.deltaY >= 0) {
+            zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
+            zoomElement.style.opacity = `${(opacity = opacity - OPACITY_STEP)}`;
+            contactIcons.style.opacity = "0";
+        } else {
+            if (zoom + ZOOM_SPEED > 1.2) {
+                zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
+                zoomElement.style.opacity = `${(opacity = opacity + OPACITY_STEP)}`;
+                contactIcons.style.opacity = "1";
             }
-        };
+        }
 
-        lockScroll("enabled");
-
-        useEffect(() => {
-            const zoomElement = document.querySelector(".text-home") as HTMLElement;
-            const contactIcons = document.querySelector(".contact-icons") as HTMLElement;
-            const OPACITY_STEP = 0.1;
-            const ZOOM_SPEED = 0.2;
-            let opacity = 1;
-            let zoom = 1;
-            let y = 0;
-
-            document.addEventListener("wheel", (e) => {
-                if (window.pageYOffset <= 0) {
-                    lockScroll("enabled");
-                    zoomElement.style.display = `block`;
-                }
-
-                if (e.deltaY >= 0) {
-                    zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
-                    zoomElement.style.opacity = `${(opacity = opacity - OPACITY_STEP)}`;
-                    contactIcons.style.opacity = "0";
-                } else {
-                    if (zoom + ZOOM_SPEED > 1.2) {
-                        zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
-                        zoomElement.style.opacity = `${(opacity = opacity + OPACITY_STEP)}`;
-                        contactIcons.style.opacity = "1";
-                    }
-                }
-
-                if (opacity <= 0) {
-                    lockScroll("disabled");
-                    zoomElement.style.display = `none`;
-                }
-            });
-        }, []);
+        if (opacity <= 0) {
+            lockScroll("disabled");
+            zoomElement.style.display = `none`;
+        }
     }
+
+    window.addEventListener("wheel", handleScroll);
+    lockScroll("enabled");
+
+    return () => {
+        window.removeEventListener('wheel', handleScroll);
+        lockScroll("disabled");
+    };
+}, []);
+    
 
     return (
         <HomeContainer>
