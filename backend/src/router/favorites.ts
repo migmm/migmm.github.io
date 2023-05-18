321c051fea07ca4a30216a69661900ed73fbce5c
migmm <micmigm@gmail.com>
2023-05-18 19:49:47 -0300

    Added dev dependences

--- backend/src/router/favorites.ts
+++ backend/src/router/favorites.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import favouriteController from '../controller/favourite';
 
-
 const routerFavourite = express.Router();
 
 routerFavourite.post('/:id', favouriteController.addFavorite);
