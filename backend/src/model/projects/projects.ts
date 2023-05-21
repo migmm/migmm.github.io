5df75e166b6ff50d725390f76e9763edd49ce881
migmm <micmigm@gmail.com>
2023-05-20 22:55:52 -0300

    Changed label

--- backend/src/model/projects/projects.ts
+++ backend/src/model/projects/projects.ts
@@ -4,7 +4,7 @@ import ProjectModelMongoDB from './projects-mongodb';
 
 class ProjectModel {
     static get(type:any, caller:any = '') {
-        console.log(`#### Users Model Persistence -> ${database.PERSISTENCE_TYPE || "by default"} ${caller ? `Called from ${caller}` : ""} ####`);
+        console.log(`#### Projects Model Persistence -> ${database.PERSISTENCE_TYPE || "by default"}${caller ?` Called from ${caller}` : ""} ####`);
         switch (type) {
             case PERSISTENCE_TYPES.TYPE_MONGODB:
                 return new ProjectModelMongoDB();
