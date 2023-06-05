fb937c5be6bc2f5bd86d3de66421d916a9c45092
migmm <micmigm@gmail.com>
2023-06-05 18:20:42 -0300

    Changed margins

--- frontend/src/Styles/H1/H1.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const H1 = styled.h1`
-    font-family: "Work Sans", sans-serif;
-    font-weight: 800;
-    text-align: center;
-    margin: 1em 1em 0.5em 1em;
-`;
---
+++ frontend/src/Styles/H1/H1.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+    font-family: 'Work Sans', sans-serif;
+    font-weight: 800;
+    text-align: center;
+    margin: 1.5em 1em 1em 1em;
+`;
