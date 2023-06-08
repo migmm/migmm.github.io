08efed8d4cda65b824b9b6ca6ac26598ad59a0d2
migmm <micmigm@gmail.com>
2023-06-07 22:01:26 -0300

    Changed Checkbox component

--- frontend/src/Styles/Form/CheckBox/CheckBox.tsx
+++ frontend/src/Styles/Form/CheckBox/CheckBox.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import styled from 'styled-components';
 
-
-interface Props {
-  isChecked: boolean;
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  label: string;
-  id: string;
-}
-
-const Checkbox = (props: Props) => {
-  return (
-    <div>
-      <input
-        type='checkbox'
-        id={props.id}
-        checked={props.isChecked}
-        onChange={props.handleChange}
-      />
-      <label htmlFor={props.id}>{props.label}</label>
-    </div>
-  );
+const Checkbox = ({ label }: any) => {
+    return (
+        <Button className='checkbox-wrapper'>
+            <input type='checkbox' id='input-check' />
+            <label htmlFor='input-check'>{label}</label>
+        </Button>
+    );
 };
 
 export default Checkbox;
 
 
+const Button = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    
+    input {
+        padding: 0;
+        height: initial;
+        width: initial;
+        margin-bottom: 0;
+        display: none;
+        cursor: pointer;
+    }
+
+    label {
+        position: relative;
+        cursor: pointer;
+        font-family: 'Work Sans', sans-serif;
+        font-weight: 500;
+        margin: 0.5em 0 0.5em 0;
+    }
+
+    label:before {
+        content: '';
+        background-color: transparent;
+        border: 2px solid #EBEBEB;
+        border-radius: 10px;
+        background-color: #ffffff;
+        padding: 10px;
+        display: inline-block;
+        position: relative;
+        vertical-align: middle;
+        cursor: pointer;
+        margin-right: 5px;
+    }
+
+    input:checked + label:after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0px;
+        left: 9px;
+        width: 6px;
+        height: 14px;
+        border: solid #000000;
+        border-width: 0 3px 3px 0;
+        transform: rotate(45deg);
+    }
+`;
--- frontend/src/views/AddProject/AddProject.tsx
+++ frontend/src/views/AddProject/AddProject.tsx
@@ -1,49 +1,50 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import axios from "axios";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-import Button from "../../Styles/Form/Button/Button";
-import Label from "../../Styles/Form/Label/Label";
-import Input from "../../Styles/Form/Input/Input";
-import Select from "../../Styles/Form/Select/Select";
-import InputFile from "../../Styles/Form/InputFile/InputFile";
-import LabelError from "../../Styles/Form/LabelError/LabelError";
-import Textarea from "../../Styles/Form/Textarea/Textarea";
-import H1 from "../../Styles/H1/H1";
-import Checkbox from "../../Styles/Form/CheckBox/CheckBox";
-import QuillEditor from "../../components/Quill/Quill";
+import Button from '../../Styles/Form/Button/Button';
+import Label from '../../Styles/Form/Label/Label';
+import Input from '../../Styles/Form/Input/Input';
+import Select from '../../Styles/Form/Select/Select';
+import InputFile from '../../Styles/Form/InputFile/InputFile';
+import LabelError from '../../Styles/Form/LabelError/LabelError';
+import Textarea from '../../Styles/Form/Textarea/Textarea';
+import H1 from '../../Styles/H1/H1';
+import Checkbox from '../../Styles/Form/CheckBox/CheckBox';
+import QuillEditor from '../../components/Quill/Quill';
+
 
 const AddProject = () => {
-    const [imagePreview, setImagePreview] = useState("");
-    const [editorHtml, setEditorHtml] = useState("");
+    const [imagePreview, setImagePreview] = useState('');
+    const [editorHtml, setEditorHtml] = useState('');
 
     const [formData, setFormData] = useState({
-        title: "",
-        errorTitle: "",
-        category: "",
-        errorCategory: "",
-        projectName: "",
-        projectStatus: "",
-        projectUrl: "",
-        shortDescription: "",
-        error: "",
+        title: '',
+        errorTitle: '',
+        category: '',
+        errorCategory: '',
+        projectName: '',
+        projectStatus: '',
+        projectUrl: '',
+        shortDescription: '',
+        error: '',
         imageCount: 0,
-        isCheckedA: false,
+        isCheckedImage: false,
     });
+
+    const handleCheckboxChange = (event : any) => {
+        const { name, checked } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: checked,
+        }));
+    };
 
     const handleChange = (event: any) => {
         const { name, value } = event.target;
         setFormData((prevFormData) => ({
             ...prevFormData,
             [name]: value,
-        }));
-    };
-
-    const handleCheckboxChange = (event: any) => {
-        const { name, checked } = event.target;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: checked,
         }));
     };
 
@@ -54,7 +55,6 @@ const AddProject = () => {
         projectStatus,
         projectUrl,
         shortDescription,
-        isCheckedA,
     } = formData;
 
     const handleEditorChange = (html: string) => {
@@ -65,11 +65,11 @@ const AddProject = () => {
         e.preventDefault();
 
         const formData = new FormData();
-        formData.append("title", title);
-        formData.append("category", category);
-
+        formData.append('title', title);
+        formData.append('category', category);
+        console.log(formData)
         axios
-            .post("/api/projects", formData)
+            .post('/api/projects', formData)
             .then((response) => {
                 
                 console.log(response.data);
@@ -82,68 +82,72 @@ const AddProject = () => {
 
     return (
         <AddProjectStyles>
-            <div className="project-container">
+            <div className='project-container'>
                 <H1>New Project</H1>
-                <div className="add-form-container">
+                <div className='add-form-container'>
                     <form onSubmit={handleSubmit}>
-                        <div className="input-group">
-                            <Label htmlFor="project-name">Project title</Label>
-                            <Input type="text" id="projectName" name="projectName" value={projectName} onChange={handleChange} />
+                        <div className='input-group'>
+                            <Label htmlFor='project-name'>Project title</Label>
+                            <Input type='text' id='projectName' name='projectName' value={projectName} onChange={handleChange} />
                             <LabelError>Error</LabelError>
-                            <Label htmlFor="project-status">Status</Label>
-                            <Select name="projectStatus" id="project-status" value={projectStatus} onChange={handleChange}>
-                                <option value="">Select status</option>
-                                <option value="inProgress">In Progress</option>
-                                <option value="finished">Finished</option>
-                                <option value="cancelled">Cancelled</option>
+
+                            <Label htmlFor='project-status'>Status</Label>
+                            <Select name='projectStatus' id='project-status' value={projectStatus} onChange={handleChange}>
+                                <option value=''>Select status</option>
+                                <option value='inProgress'>In Progress</option>
+                                <option value='finished'>Finished</option>
+                                <option value='cancelled'>Cancelled</option>
                             </Select>
                             <LabelError>Error</LabelError>
 
+                            <Checkbox 
+                                name='Include Image' 
+                                checked={formData.isCheckedImage} 
+                                onChange={handleCheckboxChange}
+                                label="Show in landing page" 
+                                />
+
                             <div
                                 style={{
-                                    fontFamily: "Work Sans",
-                                    margin: "0 auto",
-                                    padding: "0 0 1em 0",
+                                    fontFamily: 'Work Sans',
+                                    margin: '0 auto',
+                                    padding: '0 0 1em 0',
                                 }}
                             >
-                                {
-                                    <Checkbox
-                                        handleChange={handleCheckboxChange}
-                                        isChecked={isCheckedA}
-                                        label=" Show on landing page"
-                                        id="checkbox-main-project"
-                                    />
-                                }
                             </div>
                             <LabelError>Error</LabelError>
-                            <Label htmlFor="project-url">URL</Label>
-                            <Input type="text" id="project-url" name="projectUrl" value={projectUrl} onChange={handleChange} />
+
+                            <Label htmlFor='project-url'>URL</Label>
+                            <Input type='text' id='project-url' name='projectUrl' value={projectUrl} onChange={handleChange} />
                             <LabelError>Error</LabelError>
 
-                            <Label htmlFor="short-description">Short description</Label>
-                            <Textarea id="short-description" name="shortDescription" value={shortDescription} onChange={handleChange} />
+                            <Label htmlFor='short-description'>Short description</Label>
+                            <Textarea id='short-description' name='shortDescription' value={shortDescription} onChange={handleChange} />
                             <LabelError>Error</LabelError>
-                            <Label htmlFor="certification-image">Cover Image</Label>
+
+                            <Label htmlFor='certification-image'>Cover Image</Label>
                             <InputFile setImagePreview={setImagePreview} imagePreview={imagePreview} />
                             <LabelError>Error</LabelError>
-                            <Label htmlFor="project-description">Project description</Label>
+
+                            <Label htmlFor='project-description'>Project description</Label>
                             <div
                                 style={{
-                                    width: "100%",
-                                    margin: "0 auto",
+                                    width: '100%',
+                                    margin: '0 auto',
                                 }}
                                 /*   onDrop={handleDrop} */
                             >
                                 <QuillEditor 
-                                    placeholder="Enter text..." 
+                                    placeholder='Enter text...' 
                                     onChange={handleEditorChange}
                                     value={editorHtml} 
                                 />
                             </div>
                             <LabelError>Error</LabelError>
+                            
                         </div>
-                        <div className="input-group">
-                            <Button type="submit">Add</Button>
+                        <div className='input-group'>
+                            <Button type='submit'>Add</Button>
                             {/*  <Button onClick={handleReset}>Reset</Button> */}
                         </div>
                     </form>
