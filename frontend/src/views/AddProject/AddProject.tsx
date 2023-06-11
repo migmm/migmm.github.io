ee5b6eb041962bc8d9169838185247af56e8fb9c
migmm <micmigm@gmail.com>
2023-06-11 00:54:15 -0300

    Added Image validations

--- frontend/src/views/AddProject/AddProject.tsx
+++ frontend/src/views/AddProject/AddProject.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import CommonStyles from "../../Styles/CommonStyles/CommonStyles";
-import Button from "../../Styles/Form/Button/Button";
-import Label from "../../Styles/Form/Label/Label";
-import Input from "../../Styles/Form/Input/Input";
-import Select from "../../Styles/Form/Select/Select";
-import InputFile from "../../Styles/Form/InputFile/InputFile";
-import LabelError from "../../Styles/Form/LabelError/LabelError";
-import Textarea from "../../Styles/Form/Textarea/Textarea";
-import H1 from "../../Styles/H1/H1";
-import Checkbox from "../../Styles/Form/CheckBox/CheckBox";
-import InputGroup from "../../Styles/Form/InputGroup/InputGroup";
-import QuillEditor from "../../components/Quill/Quill";
-import { validations, initialFields } from "./validations";
-import { useValidation } from "../../hooks/useValidations";
-import useFormUtils from "../../hooks/useFormUtils";
+import CommonStyles from '../../Styles/CommonStyles/CommonStyles';
+import Button from '../../Styles/Form/Button/Button';
+import Label from '../../Styles/Form/Label/Label';
+import Input from '../../Styles/Form/Input/Input';
+import Select from '../../Styles/Form/Select/Select';
+import InputFile from '../../Styles/Form/InputFile/InputFile';
+import LabelError from '../../Styles/Form/LabelError/LabelError';
+import Textarea from '../../Styles/Form/Textarea/Textarea';
+import H1 from '../../Styles/H1/H1';
+import Checkbox from '../../Styles/Form/CheckBox/CheckBox';
+import InputGroup from '../../Styles/Form/InputGroup/InputGroup';
+import QuillEditor from '../../components/Quill/Quill';
+import { validations, initialFields } from './validations';
+import { useValidation } from '../../hooks/useValidations';
+import useFormUtils from '../../hooks/useFormUtils';
 import convertBase64ToBlob from '../../utils/base64toImage';
 
 const AddProject = () => {
-    const [imagePreview, setImagePreview] = useState("");
-    const [editorHtml, setEditorHtml] = useState("");
+    const [imagePreview, setImagePreview] = useState('');
+    const [editorHtml, setEditorHtml] = useState('');
 
-    const [error, setError] = useState("");
+    const [error, setError] = useState('');
     const [buttonMessage, setButtonMessage] = useState(false);
 
     const { errors, validateForm } = useValidation(validations);
@@ -33,35 +33,35 @@ const AddProject = () => {
     const handleFileChange = (imageData: any) => {
         if (imageData) {
 
-            console.log("Archivo cargado:", imageData);
+            console.log('Archivo cargado:', imageData);
         } else {
 
-            console.log("No se ha cargado ningún archivo.");
+            console.log('No se ha cargado ningún archivo.');
         }
         setImagePreview(imageData);
-        handleChange("coverImage", imageData);
+        handleChange('coverImage', imageData);
     };
 
     const handleEditorChange = (html: string) => {
         setEditorHtml(html);
-        handleChange("editorHtml", html);
+        handleChange('editorHtml', html);
     };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setError("");
+        setError('');
         setButtonMessage(true);
 
         const formData = new FormData();
 
-        console.log("fields:", fields);
+        console.log('fields:', fields);
         const base64Image = fields.coverImage;
 
         const blob = convertBase64ToBlob(base64Image, 'image/jpeg');
 
-        formData.append("coverImage", blob, "coverImage.jpg");
+        formData.append('coverImage', blob, 'coverImage.jpg');
 
-        console.log("form data");
+        console.log('form data');
         for (const [key, value] of formData.entries()) {
             console.log(`${key}:`, value);
         }
@@ -70,13 +70,13 @@ const AddProject = () => {
             showInLandPage: showInLandPage,
         };
 
-        console.log("data", data);
+        console.log('data', data);
 
         if (validateForm(fields)) {
             try {
-                const response = await axios.post("http://localhost:8080/api/users", data, {
+                const response = await axios.post('http://localhost:8080/api/users', data, {
                     headers: {
-                        "Content-Type": "application/json",
+                        'Content-Type': 'application/json',
                     },
                 });
 
@@ -105,21 +105,21 @@ const AddProject = () => {
 
     return (
         <CommonStyles>
-            <div className="project-container">
-                <H1 innerText="New Project" />
+            <div className='project-container'>
+                <H1 innerText='New Project' />
 
-                <div className="add-form-container">
+                <div className='add-form-container'>
                     <form onSubmit={handleSubmit}>
                         <InputGroup>
                             <Label
-                                htmlFor="project-name"
-                                innerText="Project title"
+                                htmlFor='project-name'
+                                innerText='Project title'
                             />
 
                             <Input
-                                type="text"
-                                id="projectName"
-                                name="projectName"
+                                type='text'
+                                id='projectName'
+                                name='projectName'
                                 value={fields.projectName}
                                 onChange={(e) => handleChange(e.target.name, e.target.value)}
                             />
@@ -128,36 +128,36 @@ const AddProject = () => {
                             />
 
                             <Label
-                                htmlFor="project-status"
-                                innerText="Status"
+                                htmlFor='project-status'
+                                innerText='Status'
                             />
                             <Select
-                                name="projectStatus"
-                                id="project-status"
+                                name='projectStatus'
+                                id='project-status'
                                 value={fields.projectStatus}
                                 onChange={(e) => handleChange(e.target.name, e.target.value)}
                             >
-                                <option value="">Select status</option>
-                                <option value="inProgress">In Progress</option>
-                                <option value="finished">Finished</option>
-                                <option value="cancelled">Cancelled</option>
+                                <option value=''>Select status</option>
+                                <option value='inProgress'>In Progress</option>
+                                <option value='finished'>Finished</option>
+                                <option value='cancelled'>Cancelled</option>
                             </Select>
                             <LabelError
                                 innerText={errors.projectStatus}
                             />
 
                             <Checkbox
-                                name="showInLandPage"
+                                name='showInLandPage'
                                 checked={showInLandPage}
                                 onChange={(isChecked: boolean) => setShowInLandPage(isChecked)}
-                                label="Show in landing page"
+                                label='Show in landing page'
                             />
 
                             <div
                                 style={{
-                                    fontFamily: "Work Sans",
-                                    margin: "0 auto",
-                                    padding: "0 0 1em 0",
+                                    fontFamily: 'Work Sans',
+                                    margin: '0 auto',
+                                    padding: '0 0 1em 0',
                                 }}
                             />
 
@@ -166,14 +166,14 @@ const AddProject = () => {
                             />
 
                             <Label
-                                htmlFor="git-url"
-                                innerText="GIT URL"
+                                htmlFor='git-url'
+                                innerText='GIT URL'
                             />
 
                             <Input
-                                type="text"
-                                id="git-url"
-                                name="gitURL"
+                                type='text'
+                                id='git-url'
+                                name='gitURL'
                                 value={fields.gitURL}
                                 onChange={(e) => handleChange(e.target.name, e.target.value)}
                             />
@@ -182,14 +182,14 @@ const AddProject = () => {
                             />
 
                             <Label
-                                htmlFor="deploy-url"
-                                innerText="Deploy URL"
+                                htmlFor='deploy-url'
+                                innerText='Deploy URL'
                             />
 
                             <Input
-                                type="text"
-                                id="deploy-url"
-                                name="deployURL"
+                                type='text'
+                                id='deploy-url'
+                                name='deployURL'
                                 value={fields.deployURL}
                                 onChange={(e) => handleChange(e.target.name, e.target.value)}
                             />
@@ -198,13 +198,13 @@ const AddProject = () => {
                             />
 
                             <Label
-                                htmlFor="short-description"
-                                innerText="Short description"
+                                htmlFor='short-description'
+                                innerText='Short description'
                             />
 
                             <Textarea
-                                id="short-description"
-                                name="shortDescription"
+                                id='short-description'
+                                name='shortDescription'
                                 value={fields.shortDescription}
                                 onChange={(e) => handleChange(e.target.name, e.target.value)}
                             />
@@ -213,32 +213,32 @@ const AddProject = () => {
                             />
 
                             <Label
-                                htmlFor="cover-image"
-                                innerText="Cover Image"
+                                htmlFor='cover-image'
+                                innerText='Cover Image'
                             />
                             <InputFile
                                 setImagePreview={handleFileChange}
                                 imagePreview={imagePreview}
-                                id="cover-image"
-                                name="coverImage"
+                                id='cover-image'
+                                name='coverImage'
                             />
                             <LabelError
                                 innerText={errors.coverImage}
                             />
 
                             <Label 
-                                htmlFor="project-description"
-                                innerText="Project description"
+                                htmlFor='project-description'
+                                innerText='Project description'
                             />
                             <div
                                 style={{
-                                    width: "100%",
-                                    margin: "0 auto",
+                                    width: '100%',
+                                    margin: '0 auto',
                                 }}
                                 /*   onDrop={handleDrop} */
                             >
                                 <QuillEditor 
-                                    placeholder="Enter text..."
+                                    placeholder='Enter text...'
                                     onChange={handleEditorChange}
                                     value={editorHtml}
                                 />
@@ -254,15 +254,15 @@ const AddProject = () => {
 
                         <InputGroup>
                             <Button
-                                type="submit"
+                                type='submit'
                                 disabled={buttonMessage}
-                                innerText={buttonMessage ? "Wait.." : "Add"}
+                                innerText={buttonMessage ? 'Wait..' : 'Add'}
                             />
 
                             <Button
-                                type="reset"
+                                type='reset'
                                 onClick={handleReset}
-                                innerText="Reset"
+                                innerText='Reset'
                             />
                         </InputGroup>
                     </form>
--- frontend/src/views/AddProject/validations.tsx
+++ frontend/src/views/AddProject/validations.tsx
@@ -56,12 +56,35 @@ export const validations = {
     coverImage: {
         required: true,
         errorMessage: 'Cover Image is required.',
-        validate: (value: any) => {
+        validate: (value: any, formData?: Record<string, string>) => {
+            
             if (!value) {
                 return 'Cover Image is required.';
             }
+
+            const isBase64PNG = value.startsWith('data:image/png;base64');
+            const isBase64JPEG = value.startsWith('data:image/jpeg;base64');
+            const isBase64JPG = value.startsWith('data:image/jpg;base64');
+            if (!isBase64PNG && !isBase64JPEG && !isBase64JPG) {
+                return 'Invalid file format. Only PNG, JPEG, and JPG files are allowed.';
+            }
+
+            const imageData = value.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+            const decodedImage = atob(imageData);
+
+            const maxSizeKB = 1024;
+            const maxSizeBytes = maxSizeKB * 1024;
+            if (decodedImage.length > maxSizeBytes) {
+                return 'File size exceeds the maximum limit of 1MB.';
+            }
+
+            const maxFileCount = 2; 
+            const currentFileCount = Object.values(formData || {}).filter((fieldValue) => fieldValue.startsWith('data:image')).length;
+            if (currentFileCount >= maxFileCount) {
+            return `You can only upload a maximum of ${maxFileCount} files.`;
+            }
             return true;
-        },
+        }
     },
     editorHtml: {
         required: true,
