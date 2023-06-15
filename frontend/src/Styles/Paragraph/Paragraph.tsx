0252a8c86ba7d057417a404e91968c2962209fb8
migmm <micmigm@gmail.com>
2023-06-14 22:09:35 -0300

    Added styles to a

--- frontend/src/Styles/Paragraph/Paragraph.tsx
+++ frontend/src/Styles/Paragraph/Paragraph.tsx
@@ -5,6 +5,18 @@ const Paragraph = styled.p`
     font-family: 'Work Sans', sans-serif;
     font-weight: 500;
     margin: 0.5em 0 0.5em 0;
+
+    a {
+        color: #000000;
+        text-decoration: none;
+
+        :hover {
+            color: #ED1B23;
+        }
+        :active {
+            color: #ED1B23;
+        }
+    }
 `;
 
 export default Paragraph;
--- frontend/src/views/ViewProject/ViewProject.tsx
+++ frontend/src/views/ViewProject/ViewProject.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FormattedView from './FormattedView';
-import sampleObject from '../../../dummy/sampleObject';
+import sampleObject from '../../dummy/sampleObject';
 
 import CommonStyles from '../../Styles/CommonStyles/CommonStyles';
 import H1 from '../../Styles/H1/H1';
@@ -13,7 +13,7 @@ const ViewProject = () => {
         <CommonStyles>
             <div>
                 <H1 innerText={sampleObject.projectName} />
-
+                <Paragraph>Short Description: {sampleObject.shortDescription}</Paragraph>
                 <Paragraph>Project Status: {sampleObject.projectStatus}</Paragraph>
                 <Paragraph>
                     Git URL: <a href={sampleObject.gitURL}>{sampleObject.gitURL}</a>
@@ -21,7 +21,7 @@ const ViewProject = () => {
                 <Paragraph>
                     Deploy URL: <a href={sampleObject.deployURL}>{sampleObject.deployURL}</a>
                 </Paragraph>
-                <Paragraph>Short Description: {sampleObject.shortDescription}</Paragraph>
+                
                 <FormattedView 
                     content={sampleObject.editorHtml} />
             </div>
