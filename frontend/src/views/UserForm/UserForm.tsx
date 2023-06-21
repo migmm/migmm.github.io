231a94eb58831db2ae27302a68cb12c3723d3897
migmm <micmigm@gmail.com>
2023-06-20 21:57:42 -0300

    Created user form layout

---
+++ frontend/src/views/UserForm/UserForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const UserForm = () => {
+    return (
+        <UserFormContent>
+            
+        </UserFormContent>
+    );
+};
+
+export default UserForm;
+
+const UserFormContent = styled.main`
+`;
