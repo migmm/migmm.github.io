2e3fe016c5e29d36bf60a8e2ea3021ee5abee9f1
migmm <micmigm@gmail.com>
2023-06-28 17:08:02 -0300

    Removed git readme

--- frontend/src/views/AddProject/AddProject.tsx
+++ frontend/src/views/AddProject/AddProject.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import ReactMarkdown from 'react-markdown';
 
 import CommonStyles from '../../Styles/CommonStyles/CommonStyles';
 import Button from '../../Styles/Form/Button/Button';
@@ -22,79 +21,68 @@ import ContainerStyles from '../../Styles/Container/Container';
 import { apiURL } from '../../config/urls';
 import ButtonGroup from '../../Styles/Form/ButtonGroup/ButtonGroup';
 
+
 const AddProject = () => {
-  const [imagePreview, setImagePreview] = useState('');
-  const [editorHtml, setEditorHtml] = useState('');
-  const formData = new FormData();
-  const [error, setError] = useState('');
-  const [buttonMessage, setButtonMessage] = useState(false);
-  const [showInLandPage, setShowInLandPage] = useState(false);
-  const [useFromGit, setUseFromGit] = useState(false);
-  const [readmeContent, setReadmeContent] = useState('');
+    const [imagePreview, setImagePreview] = useState('');
+    const [editorHtml, setEditorHtml] = useState('');
+    const formData = new FormData();
+    const [error, setError] = useState('');
+    const [buttonMessage, setButtonMessage] = useState(false);
 
-  const { errors, validateForm } = useValidation(validations);
-  const { fields, handleChange, handleReset } = useFormUtils(initialFields);
+    const { errors, validateForm } = useValidation(validations);
+    const { fields, handleChange, handleReset } = useFormUtils(initialFields);
 
-  const handleFileChange = (imageData:any) => {
-    setImagePreview(imageData);
-    handleChange('coverImage', imageData);
-  };
+    const [showInLandPage, setShowInLandPage] = useState(false);
 
-  const handleEditorChange = (html:any) => {
-    setEditorHtml(html);
-    handleChange('editorHtml', html);
-  };
+    const handleFileChange = (imageData: any) => {
+        setImagePreview(imageData);
+        handleChange('coverImage', imageData);
+    };
 
-  const handleUseFromGitChange = (isChecked:any) => {
-    setUseFromGit(isChecked);
-    if (isChecked) {
-      // Aquí puedes realizar una llamada a la API o usar una biblioteca de Markdown para obtener el contenido del archivo README.md desde la URL fields.gitURL
-      // Por simplicidad, en este ejemplo, asumimos que el contenido del archivo README.md se establece en un estado directamente
-      setReadmeContent('Contenido del archivo README.md');
-    } else {
-      setReadmeContent('');
-    }
-  };
+    const handleEditorChange = (html: string) => {
+        setEditorHtml(html);
+        handleChange('editorHtml', html);
+    };
 
-  const handleSubmit = async (e:any) => {
-    e.preventDefault();
-    setError('');
-    setButtonMessage(true);
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError('');
+        setButtonMessage(true);
 
-    const base64Image = fields.coverImage;
-    const blob = convertBase64ToBlob(base64Image, 'image/jpeg');
+        const base64Image = fields.coverImage;
+        const blob = convertBase64ToBlob(base64Image, 'image/jpeg');
 
-    formData.append('coverImage', blob, 'coverImage.jpg');
-    formData.append('showInLandPage', showInLandPage ? 'true' : 'false');
+        formData.append('coverImage', blob, 'coverImage.jpg');
+        formData.append('showInLandPage', showInLandPage ? 'true' : 'false');
 
-    for (const key in fields) {
-      if (fields.hasOwnProperty(key)) {
-        if (fields.hasOwnProperty(key) && key !== 'showInLandPage' && key !== 'coverImage') {
-          formData.append(key, fields[key]);
+        for (const key in fields) {
+            if (fields.hasOwnProperty(key)) {
+                if (fields.hasOwnProperty(key) && key !== 'showInLandPage' && key !== 'coverImage') {
+                    formData.append(key, fields[key]);
+                }
+            }
         }
-      }
-    }
 
-    console.log('-- Start Form data --');
-    for (const [key, value] of formData.entries()) {
-      console.log(`${key}:`, value);
-    }
-    console.log('-- End Form data --');
-
-    if (validateForm(fields)) {
-      try {
-        const response = await axios.post(`${apiURL}addproject`, formData, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-
-        if (response.status === 201) {
-          /*  navigate('/'); */
+        console.log('-- Start Form data --');
+        for (const [key, value] of formData.entries()) {
+            console.log(`${key}:`, value);
         }
-      } catch (error:any) {
-        if (error.response) {
-          /*   const { status, data } = error.response;
+        console.log('-- End Form data --');
+
+        if (validateForm(fields)) {
+            try {
+                const response = await axios.post(`${apiURL}addproject`, formData, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (response.status === 201) {
+                    /*  navigate('/'); */
+                }
+            } catch (error: any) {
+                if (error.response) {
+                    /*   const { status, data } = error.response;
                     if (status === 401) {
                         if (data.message === 'Existing username') {
                             setError(validations.username.existingMessage);
@@ -102,138 +90,171 @@ const AddProject = () => {
                             setError(validations.email.existingMessage);
                         }
                     }  */
-        } else {
-          setError(validations.commonError.errorMessage);
-          console.error(error);
+                } else {
+                    setError(validations.commonError.errorMessage);
+                    console.error(error);
+                }
+            }
         }
-      }
-    }
 
-    setButtonMessage(false);
-  };
+        setButtonMessage(false);
+    };
 
-  return (
-    <CommonStyles>
-      <ContainerStyles>
-        <H1 innerText='New Project' />
+    return (
+        <CommonStyles>
+            <ContainerStyles>
+                <H1 innerText='New Project' />
 
-        <div className='add-form-container'>
-          <form onSubmit={handleSubmit}>
-            <InputGroup>
-              <Label htmlFor='project-name' innerText='Project title *' />
+                <div className='add-form-container'>
+                    <form onSubmit={handleSubmit}>
+                        <InputGroup>
+                            <Label
+                                htmlFor='project-name'
+                                innerText='Project title *'
+                            />
 
-              <Input
-                type='text'
-                id='projectName'
-                name='projectName'
-                value={fields.projectName}
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-              />
-              <LabelError innerText={errors.projectName} />
+                            <Input
+                                type='text'
+                                id='projectName'
+                                name='projectName'
+                                value={fields.projectName}
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                            />
+                            <LabelError
+                                innerText={errors.projectName}
+                            />
 
-              <Label htmlFor='project-status' innerText='Status *' />
-              <Select
-                name='projectStatus'
-                id='project-status'
-                value={fields.projectStatus}
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-              >
-                <option value=''>Select status</option>
-                <option value='inProgress'>In Progress</option>
-                <option value='finished'>Finished</option>
-                <option value='cancelled'>Cancelled</option>
-              </Select>
-              <LabelError innerText={errors.projectStatus} />
+                            <Label
+                                htmlFor='project-status'
+                                innerText='Status *'
+                            />
+                            <Select
+                                name='projectStatus'
+                                id='project-status'
+                                value={fields.projectStatus}
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                            >
+                                <option value=''>Select status</option>
+                                <option value='inProgress'>In Progress</option>
+                                <option value='finished'>Finished</option>
+                                <option value='cancelled'>Cancelled</option>
+                            </Select>
+                            <LabelError
+                                innerText={errors.projectStatus}
+                            />
 
-              <Checkbox
-                name='showInLandPage'
-                checked={showInLandPage}
-                onChange={(isChecked:any) => setShowInLandPage(isChecked)}
-                label='Show in landing page'
-              />
-              <LabelError innerText={errors.useFromGit} />
+                            <Checkbox
+                                name='showInLandPage'
+                                checked={showInLandPage}
+                                onChange={(isChecked: boolean) => setShowInLandPage(isChecked)}
+                                label='Show in landing page'
+                            />
 
-              <Label htmlFor='git-url' innerText='GIT URL *' />
+                            <Label
+                                htmlFor='git-url'
+                                innerText='GIT URL *'
+                            />
 
-              <Input
-                type='text'
-                id='git-url'
-                name='gitURL'
-                value={fields.gitURL}
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-              />
-              <LabelError innerText={errors.gitURL} />
+                            <Input
+                                type='text'
+                                id='git-url'
+                                name='gitURL'
+                                value={fields.gitURL}
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                            />
+                            <LabelError
+                                innerText={errors.gitURL}
+                            />
 
-              <Label htmlFor='deploy-url' innerText='Deploy URL' />
+                            <Label
+                                htmlFor='deploy-url'
+                                innerText='Deploy URL'
+                            />
 
-              <Input
-                type='text'
-                id='deploy-url'
-                name='deployURL'
-                value={fields.deployURL}
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-              />
-              <LabelError innerText={errors.deployURL} />
+                            <Input
+                                type='text'
+                                id='deploy-url'
+                                name='deployURL'
+                                value={fields.deployURL}
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                            />
+                            <LabelError
+                                innerText={errors.deployURL}
+                            />
 
-              <Label htmlFor='short-description' innerText='Short description *' />
+                            <Label
+                                htmlFor='short-description'
+                                innerText='Short description *'
+                            />
 
-              <Textarea
-                id='short-description'
-                name='shortDescription'
-                value={fields.shortDescription}
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-              />
-              <LabelError innerText={errors.shortDescription} />
+                            <Textarea
+                                id='short-description'
+                                name='shortDescription'
+                                value={fields.shortDescription}
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                            />
+                            <LabelError
+                                innerText={errors.shortDescription}
+                            />
 
-              <Label htmlFor='cover-image' innerText='Cover Image *' />
-              <InputFile
-                setImagePreview={handleFileChange}
-                imagePreview={imagePreview}
-                id='cover-image'
-                name='coverImage'
-              />
-              <LabelError innerText={errors.coverImage} />
+                            <Label
+                                htmlFor='cover-image'
+                                innerText='Cover Image *'
+                            />
+                            <InputFile
+                                setImagePreview={handleFileChange}
+                                imagePreview={imagePreview}
+                                id='cover-image'
+                                name='coverImage'
+                            />
+                            <LabelError
+                                innerText={errors.coverImage}
+                            />
 
-              <Label htmlFor='project-description' innerText='Project description *' />
+                            <Label 
+                                htmlFor='project-description'
+                                innerText='Project description *'
+                            />
+                            <div
+                                style={{
+                                    width: '100%',
+                                    margin: '0 auto',
+                                }}
+                                /*   onDrop={handleDrop} */
+                            >
+                                <QuillEditor 
+                                    placeholder='Enter text...'
+                                    onChange={handleEditorChange}
+                                    value={editorHtml}
+                                />
+                            </div>
+                            <LabelError
+                                innerText={errors.editorHtml}
+                            />
+                        </InputGroup>
 
-              <Checkbox
-                name='useFromGit'
-                checked={useFromGit}
-                onChange={(isChecked:any) => handleUseFromGitChange(isChecked)}
-                label='Use project README.md content'
-              />
-              <LabelError innerText={errors.useFromGit} />
+                        <LabelError
+                            innerText={error}
+                        />
 
-              {!useFromGit && (
-                <QuillEditor
-                  editorHtml={editorHtml}
-                  handleEditorChange={handleEditorChange}
-                  placeholder='Enter project description'
-                />
-              )}
+                        <ButtonGroup>
+                            <Button
+                                type='submit'
+                                disabled={buttonMessage}
+                                innerText={buttonMessage ? 'Wait..' : 'Add'}
+                            />
 
-              {useFromGit && readmeContent && (
-                <div>
-                  <ReactMarkdown>{readmeContent}</ReactMarkdown>
+                            <Button
+                                type='reset'
+                                onClick={handleReset}
+                                innerText='Reset'
+                            />
+                        </ButtonGroup>
+                    </form>
                 </div>
-              )}
-
-              <LabelError innerText={errors.editorHtml} />
-
-              <ButtonGroup>
-                <Button type='submit' disabled={buttonMessage}
-                innerText={buttonMessage ? 'Creating...' : 'Create project'} />
-         
-                <Button type='reset' onClick={handleReset} innerText='Reset' />
-                
-             
-              </ButtonGroup>
-            </InputGroup>
-          </form>
-        </div>
-      </ContainerStyles>
-    </CommonStyles>
-  );
+            </ContainerStyles>
+        </CommonStyles>
+    );
 };
 
 export default AddProject;
