6403cd67f08d22f6b4c562891766b4418a616348
migmm <micmigm@gmail.com>
2023-07-02 00:33:00 -0300

    Changed footer styles

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
                     <img src={userDB.logo} alt="Logo" />
                 </div>
                 <div className="p-container">
-                    <p>miguedev</p>
+                    <h2>miguedev</h2>
                 </div>
             </div>
             <div className="second-column">
@@ -32,10 +32,29 @@ const Footer = () => {
                 </ul>
             </div>
             <div className="third-column">
-                <p></p>
-                <button></button>
-                <div>
-                    
+                <h3>Get in touch</h3>
+                <p className="footer-text">Feel free to reach out to me by sending a message now, and I'll make sure to reply as soon as possible.</p>
+                <button className="footer-button">Contact me</button>
+
+                <div className="footer-icons">
+                    <a href="http://www.github.com/project1" target="_blank" rel="noreferrer">
+                        <i className="fa-brands fa-github fa-2x"></i>
+                    </a>
+                    <a href="http://www.linkedin.com.com/profile" target="_blank" rel="noreferrer">
+                        <i className="fa-brands fa-linkedin fa-2x"></i>
+                    </a>
+                    <a href="mail@mail.com" target="_blank" rel="noreferrer">
+                        <i className="fa fa-envelope fa-2x"></i>
+                    </a>
+                    <a href="4934934934" target="_blank" rel="noreferrer">
+                        <i className="fa-brands fa-whatsapp fa-2x"></i>
+                    </a>
+                    <a href="fdfdf" target="_blank" rel="noreferrer">
+                        <i className="fa-brands fa-telegram fa-2x"></i>
+                    </a>
+                    <a href="http://www.youtube.com/project1" target="_blank" rel="noreferrer">
+                        <i className="fa-brands fa-youtube fa-2x"></i>
+                    </a>
                 </div>
             </div>
             <div className="footer-link-container">
@@ -51,11 +70,15 @@ const FooterStyles = styled.footer`
     width: 100%;
     background-color: black;
     padding: 20px;
+    font-family: "Work Sans", sans-serif;
+    color: white;
     .first-column {
         display: flex;
         flex-direction: row;
         align-items: center;
         font-family: "Work Sans", sans-serif;
+        margin: 1em;
+
         .img-container {
             img {
                 width: 65px;
@@ -70,6 +93,7 @@ const FooterStyles = styled.footer`
     .second-column {
         font-family: "Work Sans", sans-serif;
         color: white;
+        margin: 1em;
         a {
             text-decoration: none;
 
@@ -80,15 +104,37 @@ const FooterStyles = styled.footer`
         .link-list {
             list-style: none; /* Eliminar las viñetas */
             padding: 0;
-            margin: 0;
+            margin-top:1em;
 
             li {
-                margin-left: 1em;
+                margin-bottom: .5em;
 
                 a {
                     text-decoration: none;
                     color: white;
                 }
+            }
+        }
+    }
+
+    .third-column {
+        margin: 1em;
+
+        .footer-text {
+            margin: 1em 1em 1em 0;
+        }
+
+        .footer-button {
+            width: 100px;
+            height: 50px;
+            border-radius: 15px;
+            margin: 1em 1em 2em 0;
+        }
+
+        .footer-icons {
+            a {
+                color: white;
+                margin: 1em;
             }
         }
     }
