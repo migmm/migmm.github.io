d75f207d4a89f5c996509f3f8babbea9295f4b03
migmm <micmigm@gmail.com>
2023-10-13 15:36:46 -0300

    Fixed scroll and cleaned code

--- frontend/src/views/Home/scrollHandler.tsx
+++ frontend/src/views/Home/scrollHandler.tsx
@@ -1,56 +1,46 @@
 export function setupScrollHandler(contactIcons:any, zoomElement:any, heroContaierFixed:any) {
-    const lockScroll = (option = 'disabled') => {
-        if (option === 'enabled') {
-            document.body.style.overflow = 'hidden';
-            document.body.style.userSelect = 'none';
-        } else {
-            document.body.style.overflow = 'auto';
-            document.body.style.userSelect = 'auto';
-        }
-    };
-
-    const OPACITY_STEP = 0.01;
-    const ZOOM_SPEED = 0.02;
+    const OPACITY_STEP = 0.1;
+    const ZOOM_SPEED = 0.2;
     let opacity = 1;
     let zoom = 1;
 
     let lastScrollY = 0;
     const POSITION_TO_FIX_TEXT = 400;
+    const POSITION_TO_APPEAR_TEXT = 100;
 
     function handleScroll(e:any) {
         const scrollY = window.scrollY;
         const deltaY = scrollY - lastScrollY;
 
-        console.log(scrollY)
-
         if (deltaY > 0) {
             zoom += ZOOM_SPEED;
             opacity -= OPACITY_STEP;
+
             if (zoom > 1.2) {
                 zoom = 1.2;
                 opacity = 0;
             }
             contactIcons.style.opacity = 0;
-
         } else if (deltaY < 0) {
             zoom -= ZOOM_SPEED;
             opacity += OPACITY_STEP;
+
             if (zoom < 1) {
                 zoom = 1;
                 opacity = 1;
             }
+
             contactIcons.style.opacity = 1;
         }
 
         if (scrollY <= POSITION_TO_FIX_TEXT) {
-            lockScroll('enabled');
             heroContaierFixed.style.position = 'fixed';
-            heroContaierFixed.style.top = '250';
+            heroContaierFixed.style.top = '250px'; // You missed 'px' here
             heroContaierFixed.style.display = 'block';
         } else {
-            lockScroll('enabled');
-            heroContaierFixed.style.position = 'fixed';
-            heroContaierFixed.style.top = '250';
+            heroContaierFixed.style.position = 'relative'; // Change to relative to allow normal flow
+            heroContaierFixed.style.top = '0';
+            heroContaierFixed.style.display = 'none'; // Hide the fixed container
         }
 
         zoomElement.style.transform = `scale(${zoom})`;
@@ -58,19 +48,18 @@ export function setupScrollHandler(contactIcons:any, zoomElement:any, heroContai
 
         lastScrollY = scrollY;
 
-        if (scrollY <= 0) {
-            lockScroll('enabled');
-            zoomElement.style.display = 'block';
+        if (scrollY <= POSITION_TO_APPEAR_TEXT) {
+            // Show the text when scrollY is 100 or less
+            heroContaierFixed.style.display = 'block';
         } else {
-            lockScroll('disabled');
+            // Hide the text when scrollY is more than 100
+            heroContaierFixed.style.display = 'none';
         }
     }
 
     window.addEventListener('scroll', handleScroll);
-    lockScroll('enabled');
 
     return () => {
         window.removeEventListener('scroll', handleScroll);
-        lockScroll('disabled');
     };
 }
