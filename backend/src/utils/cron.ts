7404dcd1729e0ea941e07e420304e780c86a2ce5
migmm <micmigm@gmail.com>
2023-11-07 17:59:55 -0300

    Added error counter

--- backend/src/utils/cron.ts
+++ backend/src/utils/cron.ts
@@ -1,70 +1,68 @@
-import axios from "axios";
-import cheerio from "cheerio";
-import cron from "node-cron";
-import api from "../api/projects";
+import axios from 'axios';
+import cheerio from 'cheerio';
+import cron from 'node-cron';
+import api from '../api/projects';
 
-let urlsArray:any = [];
+let urlsArray: string[] = [];
+let urlsArrayWithError: string[] = [];
 const maxWaitTime = 60000;
 
-
 const getProjects = async () => {
-    const projects = await api.getProjects();
-    urlsArray = [];
-
     try {
-        projects.forEach((element: { deployURL: string }) => {
-            if (element.deployURL) {
-                urlsArray.push(element.deployURL);
-            }
-        });
-
+        const projects = await api.getProjects();
+        urlsArray = projects.filter((element: any) => element.deployURL).map((element: any) => element.deployURL);
     } catch (error) {
-        console.log("Error getting projects", error);
+        console.log('Error getting projects', error);
     }
 };
 
-function fetchData(url: string, startTime: number): void {
+const fetchData = async (url: string, startTime: number): Promise<void> => {
     try {
         const currentTime = Date.now();
 
         if (currentTime - startTime > maxWaitTime) {
-            console.log("Max time reached to", url, "Stoping search.");
+            console.log('Max time reached for', url, 'Stopping search.');
+            urlsArrayWithError.push(url);
             return;
         }
 
-        axios
-            .get(url)
-            .then((response: any) => {
-                if (response.status === 200) {
-                    const html = response.data;
-                    const $ = cheerio.load(html);
+        try {
+            const response = await axios.get(url);
+            if (response.status === 200) {
+                const html = response.data;
+                const $ = cheerio.load(html);
 
-                    const pageTitle = $("title").text();
-                    console.log("Page title", url + ":", pageTitle);
-                } else {
-                    console.error("The request returned code 200 for: ", url);
-                }
-            })
-            .catch(() => {
-                console.error("HTTP request error from: ", url);
-            });
+                const pageTitle = $('title').text();
+                console.log('Page title for', url + ':', pageTitle);
+            } else {
+                console.error('The request returned status code', response.status, 'for:', url);
+                urlsArrayWithError.push(url);
+            }
+        } catch (error) {
+            console.log(url);
+            urlsArrayWithError.push(url);
+            console.error('HTTP request error for:', url);
+        }
     } catch (error) {
-        console.error("Error fetching data from: ", url);
+        urlsArrayWithError.push(url);
+        console.error('Error fetching data from:', url);
     }
-}
+};
 
-//"0 */12 * * *"
 const checkWebsOnline = () => {
-    cron.schedule("*/1 * * * *", async () => {
+    cron.schedule('*/1 * * * *', async () => {
         const startTime = Date.now();
 
         await getProjects();
 
-        urlsArray.forEach((url:any) => {
-            fetchData(url, startTime);
-        });
+        const fetchPromises = urlsArray.map((url) => fetchData(url, startTime));
+        await Promise.all(fetchPromises);
+
+        if (urlsArrayWithError.length > 0) {
+            console.log('Number of URLs with errors:', urlsArrayWithError.length);
+            urlsArrayWithError = [];
+        }
     });
 };
-
 
 export default checkWebsOnline;
