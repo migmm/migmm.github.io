1411e84e7e024dd538e160173dc7316d8d0a8cd4
migmm <micmigm@gmail.com>
2023-11-13 18:59:13 -0300

    Aded verification to certifications

--- backend/src/model/validators/certification.ts
+++ backend/src/model/validators/certification.ts
@@ -1,20 +1,13 @@
 import Joi from 'joi';
 
-class CertificationValidator {
-    static validate(certification: object) {
-        const certificationSchema = Joi.object({
-            courseTitle: Joi.string().required(),
-            description: Joi.string().required(),
-            issueDate: Joi.date(),
-            vendor: Joi.string().required(),
-            urlCheck: Joi.string().required(),
-            courseImage: Joi.string().required(),
-        });
-        const { error } = certificationSchema.validate(certification);
-        return error;
-    }
-}
+const certificationSchema = Joi.object({
+    courseTitle: Joi.string().required(),
+    description: Joi.string().required(),
+    issueDate: Joi.date(),
+    vendor: Joi.string().required(),
+    urlCheck: Joi.string().required(),
+    courseImage: Joi.string().required(),
+    type: Joi.string().required(),
+});
 
-export default CertificationValidator;
-
-
+export default certificationSchema;
--- backend/src/router/certifications.ts
+++ backend/src/router/certifications.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import certificationsController from '../controller/certifications';
+import validationMiddleware from '../middlewares/validationMiddleware';
+import certificationSchema from '../model/validators/certification';
 
 const routerCertifications = express.Router();
 
+const validateCertification = validationMiddleware(certificationSchema);
+
 routerCertifications.get('/', certificationsController.getCertifications);
 routerCertifications.get('/:id', certificationsController.getCertification);
-routerCertifications.post('/', certificationsController.postCertification);
-routerCertifications.put('/:id',  certificationsController.putCertification);
+routerCertifications.post('/', validateCertification, certificationsController.postCertification);
+routerCertifications.put('/:id', validateCertification, certificationsController.putCertification);
 routerCertifications.delete('/:id', certificationsController.deleteCertification);
 
 export default routerCertifications;
