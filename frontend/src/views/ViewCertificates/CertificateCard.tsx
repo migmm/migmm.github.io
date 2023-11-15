7cc7d7ecf451d768f7d529660c70853a944b015b
migmm <micmigm@gmail.com>
2023-11-15 18:34:51 -0300

    Added target blank to certificate verification

--- frontend/src/views/ViewCertificates/CertificateCard.tsx
+++ frontend/src/views/ViewCertificates/CertificateCard.tsx
@@ -1,30 +1,28 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 import H2 from '../../Styles/H2/H2';
-
 
 const CertificateCard = ({ courseTitle, courseImage, urlCheck }: any) => {
     return (
         <CertificateCardStyles>
-        <a href={urlCheck}>
-            <div className="card">
-                <div className="images-container">
-                    <img src={courseImage} alt="" />
+            <a href={urlCheck} target="_blank" rel="noopener noreferrer">
+                <div className="card">
+                    <div className="images-container">
+                        <img src={courseImage} alt="" />
+                    </div>
+                    <div className="favicon-container"></div>
+                    <H2 innerText={courseTitle} />
                 </div>
-                <div className="favicon-container"></div>
-                <H2 innerText={courseTitle} />
-            </div>
-        </a>
+            </a>
         </CertificateCardStyles>
     );
 };
 
 export default CertificateCard;
 
-
 const CertificateCardStyles = styled.div`
-a {
-    text-decoration: none;
-    color: black;
-}
+    a {
+        text-decoration: none;
+        color: black;
+    }
 `;
