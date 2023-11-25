1aaf5643230d727b176e06cc568af984b4d6aea0
migmm <micmigm@gmail.com>
2023-11-25 20:40:47 -0300

    Added try catch

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -6,7 +6,6 @@ import dotEnvExtended from 'dotenv-extended';
 
 dotEnvExtended.load();
 
-
 interface User {
     id: string;
     username: string;
@@ -16,80 +15,85 @@ interface User {
     banned: string;
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
 //                               GET Controllers                              //
 ////////////////////////////////////////////////////////////////////////////////
 
-const getAuth = async (_req:Request, res:Response) => {
-    //const id:any = req.params.id;
+const getAuth = async (_req: Request, res: Response) => {
     const auth = await api.getAuth('username', 'username');
-    console.log('auth controller', auth)
+    console.log('auth controller', auth);
+
     try {
         res.status(200).json(auth);
     } catch (error) {
-        res.status(500).send('Error getting auth')
+        res.status(500).send('Error getting auth');
     }
 };
-
 
 ///////////////////////////////////////////////////////////////////////////////
 //                              POST Controllers                             //
 ///////////////////////////////////////////////////////////////////////////////
 
-const postAuth = async (req:Request, res:Response) => {
-    const { username, password } = req.body
-    console.log(username, password)
-    if (!username || !password) {
-        return res.status(400).json({ message: 'All fields are required' })
+const postAuth = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    console.log(username, password);
+
+    try {
+        if (!username || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        const foundUser = (await api.getAuth('username', username)) as User;
+        console.log('user auth', foundUser);
+
+        if (!foundUser) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const match = await comparePasswords(password, foundUser.password);
+
+        if (!match) return res.status(401).json({ message: 'Unauthorized' });
+
+        const secretKey: string = process.env.ACCESS_TOKEN_SECRET as string;
+
+        const accessToken = jwt.sign({ id: foundUser.id }, secretKey, { expiresIn: '1d' });
+
+        const cookieOptions: any = {
+            //httpOnly: true,
+            sameSite: 'none',
+            //secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        };
+
+        return res.cookie('jwt', accessToken, cookieOptions).status(201).json({ accessToken });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    const foundUser = await api.getAuth('username',username) as User; 
-    console.log('user auth', foundUser);
-    
-    if(!foundUser) {
-        return res.status(401).json({ message: 'Unauthorized' })
-    }
-
-    const match = await comparePasswords(password, foundUser.password);
-
-    if (!match) return res.status(401).json({ message: 'Unauthorized' })
-
-    const secretKey:string = process.env.ACCESS_TOKEN_SECRET as string;
-
-    const accessToken = jwt.sign(
-        {  'id' : foundUser.id },
-        secretKey,
-        { expiresIn: '1d' }
-    );
-
-    const cookieOptions:any = {
-        //httpOnly: true,
-        sameSite: 'none',
-        //secure: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    };
-
-    return res.cookie('jwt', accessToken, cookieOptions).status(201).json({ accessToken });
 };
 
-
-const logout = async (req:Request, res:Response) => {
+const logout = async (req: Request, res: Response) => {
     const cookies = await req.cookies;
     console.log(req.cookies);
 
-    console.log('cookies',cookies)
+    console.log('cookies', cookies);
+    try {
+        if (!cookies?.jwt) {
+            return res.status(204).json({ error: 'Cookie not found' });
+        }
 
-    if (!cookies?.jwt) return res.sendStatus(204).json({ error: 'Cookie not found' })
+        res.clearCookie('jwt', {
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true,
+        });
 
-    res.clearCookie('jwt', { 
-        httpOnly: true, 
-        sameSite: 'none', 
-        secure: true 
-    })
-
-    return res.sendStatus(200).json({ message: 'Cookie cleared' })
-}
+        return res.status(200).json({ message: 'Cookie cleared' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
 
 
 export default {
