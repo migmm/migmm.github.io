19c39141479419988cfe063e6c7dcdef1d920941
migmm <micmigm@gmail.com>
2023-11-28 12:46:26 -0300

    Removed in case of

--- frontend/src/config/urls.tsx
+++ frontend/src/config/urls.tsx
@@ -1,2 +1,2 @@
 console.log(process.env.REACT_APP_API_URL)
-export const apiURL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api/';
+export const apiURL = process.env.REACT_APP_API_URL || '';
