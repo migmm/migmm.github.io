42a18da7cf5f5e4b531b60d97fae200f5b15e847
migmm <micmigm@gmail.com>
2023-11-28 12:44:53 -0300

    Moved url api path to env and divided prod and dev

--- frontend/src/config/urls.tsx
+++ frontend/src/config/urls.tsx
@@ -1,2 +1,2 @@
-export const apiURL = 'http://localhost:8080/api/';
-
+console.log(process.env.REACT_APP_API_URL)
+export const apiURL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api/';
