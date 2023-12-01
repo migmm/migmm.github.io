6a50168d0c62b4fdbde808f142a99f9106722e4f
Miguel <miguelm@hotmail.com.ar>
2023-11-30 21:59:17 -0300

    Removed required in deployUrl

--- backend/src/model/validators/project.ts
+++ backend/src/model/validators/project.ts
@@ -6,7 +6,7 @@ const projectSchema = Joi.object({
     projectStatus: Joi.string().required(),
     showInLandPage: Joi.boolean(),
     gitURL: Joi.string().uri().required(),
-    deployURL: Joi.string().uri().required(),
+    deployURL: Joi.string().uri(),
     shortDescription: Joi.string().required(),
     coverImage: Joi.string().required(),
     editorHtml: Joi.string().required(),
