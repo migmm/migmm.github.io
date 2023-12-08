9650b437fb37332190680b023db2eb4eedf8ee4f
Miguel <miguelm@hotmail.com.ar>
2023-12-08 18:46:55 -0300

    Added default env variable

--- backend/src/utils/JWTtoken.ts
+++ backend/src/utils/JWTtoken.ts
@@ -3,10 +3,12 @@ import dotEnvExtended from 'dotenv-extended';
 
 dotEnvExtended.load();
 
+const ACCESS_TOKEN_SECRET: string = process.env.REFRESH_TOKEN_SECRET || '';
+
 
 // USAGE 
 // const token = generateToken(user, '2h', ACCESS_TOKEN_SECRET);
-const generateToken = (user: { id: String; role: string }, expiresIn: string = '1h', type: string = 'ACCESS_TOKEN_SECRET'): string => {
+const generateToken = (user: { id: String; role: string }, expiresIn: string = '1h', type: string = ACCESS_TOKEN_SECRET): string => {
     const payload = {
         id: user.id,
         email: user.role,
