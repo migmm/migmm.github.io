494244295604986d46a3d715090a4968b95d400d
Miguel <miguelm@hotmail.com.ar>
2023-12-08 18:47:45 -0300

    Added try catch

--- backend/src/utils/JWTtoken.ts
+++ backend/src/utils/JWTtoken.ts
@@ -8,15 +8,21 @@ const ACCESS_TOKEN_SECRET: string = process.env.REFRESH_TOKEN_SECRET || '';
 
 // USAGE 
 // const token = generateToken(user, '2h', ACCESS_TOKEN_SECRET);
-const generateToken = (user: { id: String; role: string }, expiresIn: string = '1h', type: string = ACCESS_TOKEN_SECRET): string => {
-    const payload = {
-        id: user.id,
-        email: user.role,
-    };
+const generateToken = (user: { id: string; role: string }, expiresIn: string = '1h', type: string = ACCESS_TOKEN_SECRET): string => {
+    try {
+        const payload = {
+            id: user.id,
+            email: user.role,
+        };
 
-    const token = jwt.sign(payload, type, { expiresIn });
+        const token = jwt.sign(payload, type, { expiresIn });
 
-    return token;
+        return token;
+    } catch (error: any) {
+        // Handle the error, log it, or throw a more specific error if needed
+        console.error(`Error generating token: ${error.message}`);
+        throw new Error('Failed to generate token');
+    }
 };
 
 // USAGE 
@@ -29,7 +35,6 @@ const verifyToken = async (token: string, refreshTokenSecret: string): Promise<a
         throw error;
     }
 };
-
 
 
 export {
