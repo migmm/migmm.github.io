eb2d6b078ae21eaa9998e22c5e411ac27f0f5567
Miguel <miguelm@hotmail.com.ar>
2023-12-08 18:49:21 -0300

    Added description

--- backend/src/utils/JWTtoken.ts
+++ backend/src/utils/JWTtoken.ts
@@ -6,8 +6,14 @@ dotEnvExtended.load();
 const ACCESS_TOKEN_SECRET: string = process.env.REFRESH_TOKEN_SECRET || '';
 
 
-// USAGE 
-// const token = generateToken(user, '2h', ACCESS_TOKEN_SECRET);
+/**
+ * Generates a JSON Web Token (JWT) based on the provided user information.
+ *
+ * @param {Object} user - User information including id and role.
+ * @param {string} [expiresIn='1h'] - Expiration time for the token. Default is 1 hour.
+ * @param {string} [type=ACCESS_TOKEN_SECRET] - Type of token to generate. Default is access token.
+ * @returns {string} - The generated JWT.
+ */
 const generateToken = (user: { id: string; role: string }, expiresIn: string = '1h', type: string = ACCESS_TOKEN_SECRET): string => {
     try {
         const payload = {
@@ -19,14 +25,19 @@ const generateToken = (user: { id: string; role: string }, expiresIn: string = '
 
         return token;
     } catch (error: any) {
-        // Handle the error, log it, or throw a more specific error if needed
         console.error(`Error generating token: ${error.message}`);
         throw new Error('Failed to generate token');
     }
 };
 
-// USAGE 
-// const verifyToken = verifyToken(token, REFRESH_TOKEN_SECRET);
+/**
+ * Verifies a JSON Web Token (JWT) using the provided token and secret.
+ *
+ * @param {string} token - The JWT to verify.
+ * @param {string} refreshTokenSecret - The secret key used to verify the token.
+ * @returns {Promise<any>} - A Promise that resolves to the decoded token if verification is successful.
+ * @throws {Error} - Throws an error if the verification fails.
+ */
 const verifyToken = async (token: string, refreshTokenSecret: string): Promise<any> => {
     try {
         const decodedToken = jwt.verify(token, refreshTokenSecret);
