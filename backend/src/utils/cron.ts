4a147591ed56da1bf82892dbf476efb9fe7aff29
Miguel <miguelm@hotmail.com.ar>
2023-12-13 22:06:02 -0300

    Refactored cron function

--- backend/src/utils/cron.ts
+++ backend/src/utils/cron.ts
@@ -7,8 +7,8 @@ import dotEnvExtended from 'dotenv-extended';
 import convertToCronExpression from './convertToCronExpression';
 import { generateErrorHtml } from './emailContentGenerator';
 
-
 dotEnvExtended.load();
+
 
 const EMAIL_TO_SEND_MSG = process.env.EMAIL_SEND_MESSAGE || '';
 const WEBCHECK_INTERVAL = parseInt(process.env.WEBCHECK_INTERVAL || '720');
@@ -18,63 +18,76 @@ let projectTitles: string[] = [];
 let urlsArrayWithError: string[] = [];
 const maxWaitTime = 60000;
 
-const getProjects = async () => {
-    try {
-        const projects = await api.getProjects();
-        projectTitles = projects
-            .filter(
-                (element: any) => element.deployURL !== '' && element.deployURL != null
-            )
-            .map((element: any) => element.titleCheck);
-
-        urlsArray = projects
-            .filter(
-                (element: any) => element.deployURL !== '' && element.deployURL != null
-            )
-            .map((element: any) => element.deployURL);
-    } catch (error) {
-        console.log('Error getting projects', error);
-    }
-};
-
 const fetchData = async (url: string, startTime: number): Promise<void> => {
     try {
         const currentTime = Date.now();
 
         if (currentTime - startTime > maxWaitTime) {
-            console.log('Max time reached for', url, 'Stopping search.');
-            urlsArrayWithError.push(url);
+            handleMaxWaitTimeExceeded(url);
             return;
         }
 
-        try {
-            const response = await axios.get(url);
-            if (response.status === 200) {
-                const html = response.data;
-                const $ = cheerio.load(html);
+        const response = await axios.get(url);
 
-                const pageTitle = $('title').text();
-                console.log('Page title for', url + ':', pageTitle);
-
-                const index = urlsArray.indexOf(url);
-
-                if (index !== -1 && projectTitles[index] !== pageTitle) {
-                    console.error(`Error: Title mismatch for ${url}. Expected "${projectTitles[index]}", but got "${pageTitle}"`);
-                    urlsArrayWithError.push(url);
-                }
-            } else {
-                console.error('The request returned status code', response.status, 'for:', url);
-                urlsArrayWithError.push(url);
-            }
-        } catch (error) {
-            console.log(url);
-            urlsArrayWithError.push(url);
-            console.error('HTTP request error for:', url);
+        if (response.status === 200) {
+            handleSuccessfulResponse(url, response.data);
+        } else {
+            handleErrorResponse(url, response.status);
         }
     } catch (error) {
-        urlsArrayWithError.push(url);
-        console.error('Error fetching data from:', url);
+        handleRequestError(url);
     }
+};
+
+const handleMaxWaitTimeExceeded = (url: string) => {
+    console.log('Max time reached for', url, 'Stopping search.');
+    urlsArrayWithError.push(url);
+};
+
+const handleSuccessfulResponse = (url: string, html: string) => {
+    const $ = cheerio.load(html);
+    const pageTitle = $('title').text();
+    console.log('Page title for', url + ':', pageTitle);
+
+    const index = urlsArray.indexOf(url);
+
+    if (index !== -1 && projectTitles[index] !== pageTitle) {
+        handleTitleMismatch(url, projectTitles[index], pageTitle);
+    }
+};
+
+const handleErrorResponse = (url: string, statusCode: number) => {
+    console.error('The request returned status code', statusCode, 'for:', url);
+    urlsArrayWithError.push(url);
+};
+
+const handleRequestError = (url: string) => {
+    urlsArrayWithError.push(url);
+    console.error('HTTP request error for:', url);
+};
+
+const handleTitleMismatch = (url: string, expectedTitle: string, actualTitle: string) => {
+    console.error(`Error: Title mismatch for ${url}. Expected "${expectedTitle}", but got "${actualTitle}"`);
+    urlsArrayWithError.push(url);
+};
+
+const getProjects = async () => {
+    try {
+        const projects = await api.getProjects();
+        projectTitles = projects
+            .filter((element: any) => element.deployURL !== '' && element.deployURL != null)
+            .map((element: any) => element.titleCheck);
+
+        urlsArray = projects
+            .filter((element: any) => element.deployURL !== '' && element.deployURL != null)
+            .map((element: any) => element.deployURL);
+    } catch (error) {
+        handleGetProjectsError(error);
+    }
+};
+
+const handleGetProjectsError = (error: any) => {
+    console.log('Error getting projects', error);
 };
 
 const checkWebsOnline = () => {
@@ -88,19 +101,27 @@ const checkWebsOnline = () => {
             try {
                 await fetchData(url, startTime);
             } catch (error) {
-                console.error(`Error fetching data from ${url}:`, error);
-                urlsArrayWithError.push(url);
+                handleFetchDataError(url, error);
             }
         }
 
         if (urlsArrayWithError.length > 0) {
-            console.log('Number of URLs with errors:', urlsArrayWithError.length);
-            const htmlContent = generateErrorHtml(urlsArrayWithError);
-            console.log(htmlContent);
-            await sendMail(EMAIL_TO_SEND_MSG, 'Web errors', htmlContent);
-            urlsArrayWithError = [];
+            handleErrorsAfterCheck();
         }
     });
+};
+
+const handleFetchDataError = (url: string, error: any) => {
+    console.error(`Error fetching data from ${url}:`, error);
+    urlsArrayWithError.push(url);
+};
+
+const handleErrorsAfterCheck = async () => {
+    console.log('Number of URLs with errors:', urlsArrayWithError.length);
+    const htmlContent = generateErrorHtml(urlsArrayWithError);
+    console.log("Sending mail with errors...");
+    await sendMail(EMAIL_TO_SEND_MSG, 'Web errors', htmlContent);
+    urlsArrayWithError = [];
 };
 
 
