afa7a02292983281fa3bff8141d30d425e59f84b
Miguel <miguelm@hotmail.com.ar>
2023-12-13 22:27:34 -0300

    Added documentation

--- backend/src/utils/mailSender.ts
+++ backend/src/utils/mailSender.ts
@@ -1,11 +1,17 @@
 import nodemailer from 'nodemailer';
 import { smtpConfig, fromEmail } from '../config/smtpConfig';
 
-
 const transporter = nodemailer.createTransport(smtpConfig);
 
-// USAGE
-// sendmail(email, subject, content)
+
+/*
+ * Function to send mail
+ * @param {string} email - The email to send the message
+ * @param {string} subject - The subject of the mail
+ * @param {string} content - The content of the mail
+ * @returns {void}
+ */
+
 const sendMail= async (email: string, subject: string, content: string): Promise<void> => {
     try {
         const mailOptions = {
@@ -20,5 +26,6 @@ const sendMail= async (email: string, subject: string, content: string): Promise
         throw new Error('Error sending email');
     }
 };
+
 
 export default sendMail;
