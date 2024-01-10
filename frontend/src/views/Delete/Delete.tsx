17c9bb6d1262fe2bc957f4c475e3acb00476d6d1
Miguel <miguelm@hotmail.com.ar>
2024-01-09 22:25:38 -0300

    Added limitation to specific role

--- frontend/src/views/Delete/Delete.tsx
+++ frontend/src/views/Delete/Delete.tsx
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { apiURL } from '../../config/urls';
+import { useAppUser } from '../../context/UserContext';
 
 const Delete = ({ id, type }: any) => {
     const navigate = useNavigate();
+    const { role } = useAppUser();
 
     useEffect(() => {
+        if (role === null || role !== 'admin') {
+            navigate('/');
+        }
+
         const fetchData = async () => {
             try {
                 console.log('send');
