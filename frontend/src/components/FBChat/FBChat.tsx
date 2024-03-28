554a46367eba8523c84235d4e05919b8415e4215
Miguel <micmigm@gmail.com>
2024-03-28 18:31:31 -0300

    Fixed FBChat

--- frontend/src/components/FBChat/FBChat.tsx
+++ frontend/src/components/FBChat/FBChat.tsx
@@ -1,42 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { FacebookProvider, CustomChat } from 'react-facebook';
 
 interface FBChatProps {
     pageId: string;
 }
 
-
 const FBChat: React.FC<FBChatProps> = ({ pageId }) => {
-    React.useEffect(() => {
-        ((d, s, id) => {
-            const fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            const js = d.createElement(s) as HTMLScriptElement;
-            js.id = id;
-            js.src = `https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js`;
-            js.async = true;
-            js.defer = true;
-            fjs.parentNode?.insertBefore(js, fjs);
-        })(document, 'script', 'facebook-jssdk');
-    }, []);
-
     return (
         <FBChatButtonContainer>
-            <div id="fb-root">
-                <div
-                    className="fb-customerchat"
-                    data-attribution="setup_tool"
-                    data-page_id={pageId}
-                    data-theme_color="#0084ff"
-                    data-logged_in_greeting="¡Hola! ¿Cómo podemos ayudarte?"
-                    data-logged_out_greeting="¡Hola! ¿Cómo podemos ayudarte?"
-                ></div>
-            </div>
+            <FacebookProvider appId="1057521941935363" chatSupport>
+                <CustomChat pageId="184581498068766" minimized={false} />
+            </FacebookProvider>
         </FBChatButtonContainer>
     );
 };
-
 
 const FBChatButtonContainer = styled.div`
     position: fixed;
@@ -46,6 +24,5 @@ const FBChatButtonContainer = styled.div`
     height: 80px;
     z-index: 1000;
 `;
-
 
 export default FBChat;
